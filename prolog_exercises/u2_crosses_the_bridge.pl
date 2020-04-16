2389558e62548d7e876636e832393d723c70efeb
David Tonhofer <ronerycoder@gluino.name>
2020-04-16 13:28:02 +0200

    Review

--- prolog_exercises/u2_crosses_the_bridge.pl
+++ prolog_exercises/u2_crosses_the_bridge.pl
@@ -1,13 +1,49 @@
-% 2019-08-05 ronerycoder@gluino.name
-% Licensed under the Unlicense: 
-% For more information, please refer to <http://unlicense.org/>
-%
-% A problem from:
+% ===
+% 2019-08-05
+% ===
+% This is free and unencumbered software released into the public domain.
 % 
-% "Guess Lazily! making a program guess and guess well"
-% http://okmij.org/ftp/kakuritu/logic-programming.html
-% Strange Loop 2012
-% St Louis, MO September 25, 2012
+% Anyone is free to copy, modify, publish, use, compile, sell, or
+% distribute this software, either in source code form or as a compiled
+% binary, for any purpose, commercial or non-commercial, and by any
+% means.
+% 
+% For more information, please refer to <http://unlicense.org/>
+% ===
+% References
+% - - - - - -
+%
+% Torsten Sillke has collected a onster page of references:
+%
+%   "Crossing the bridge in an hour"
+%   Torsten Sillke, June 1997/Sept 2001
+%   https://www.math.uni-bielefeld.de/~sillke/PUZZLES/crossing-bridge
+%
+% A problem evoked by Oleg Kiselyov in:
+% 
+%   "Guess Lazily! making a program guess and guess well"
+%   http://okmij.org/ftp/kakuritu/logic-programming.html
+%   Strange Loop 2012
+%   St Louis, MO September 25, 2012
+%
+%     ...where it is not attacked with Prolog but used as a
+%     example to explain HANSEI.
+%
+% Paper for the general case:
+%
+%   "Crossing the Bridge at Night"
+%   by Günter Rote (2002-08-21)
+%   http://page.mi.fu-berlin.de/rote/Papers/pdf/Crossing+the+bridge+at+night.pdf
+%
+% Regularly appears on StackOverflow
+%
+%   https://stackoverflow.com/questions/33469487/prolog-river-crossing
+%   https://stackoverflow.com/questions/32978566/bridge-crossing-puzzle-with-clpfd
+%   https://stackoverflow.com/questions/10131517/prolog-bridge-puzzle-with-unlimited-people
+%   https://stackoverflow.com/questions/1144207/bridge-crossing-puzzle
+%
+% Description
+% - - - - - -
 %
 % ”U2” has a concert that starts in 17 minutes and they must all
 % cross a bridge to get there. They stand on the same side of the
@@ -40,28 +76,34 @@
 % minutes to develop (compiled and ran on the 1st try though).
 % Another guy solved it in three minutes. A group of 50, at Motorola,
 % couldn’t figure it out at all.
+% ===
+
+% ----8<----8<----8<----8<----8<----8<----8<----8<----8<----
 
 % This program took about 2h to write. I'm not ready for Microsoft.
 % 
-% Run with ?- solve(X).
+% Run with 
+% 
+%    set_prolog_flag(answer_write_options,[max_depth(100)]).
+%    solve(X).
 % 
 % This gives the following solutions (note that the "left group"
 % is always the one having the flashlight and the "target side" of
 % the bridge switches on ever expanded state)
 % 
-% X = [ state([adam, larry, bono, edge], [], 17),  flashlight is with [adam, larry, bono, edge] on target side 
-%       state([bono, edge], [adam, larry], 15),    flashlight is with [bono, edge] on wrong side 
-%       state([edge, adam, larry], [bono], 13),    flashlight is with [edge, adam, larry] on target side 
-%       state([adam, larry, bono], [edge], 3),     flashlight is with [adam, larry, bono] on wrong side 
-%       state([bono, edge], [adam, larry], 2),     flashlight is with [bono,edge] on target side 
-%       state([bono, edge, adam, larry], [], 0)]   START, flashlight on wrong side
+% X = [ state([adam, larry, bono, edge], [], 17),  - flashlight is with [adam, larry, bono, edge] on target side 
+%       state([bono, edge], [adam, larry], 15),    - flashlight is with [bono, edge] on wrong side 
+%       state([edge, adam, larry], [bono], 13),    - flashlight is with [edge, adam, larry] on target side 
+%       state([adam, larry, bono], [edge], 3),     - flashlight is with [adam, larry, bono] on wrong side 
+%       state([bono, edge], [adam, larry], 2),     - flashlight is with [bono,edge] on target side 
+%       state([bono, edge, adam, larry], [], 0)]   - START, flashlight on wrong side
 %       
-% X = [ state([adam, larry, bono, edge], [], 17),  flashlight is with [adam, larry, bono, edge] on target side 
-%       state([edge, bono], [adam, larry], 15),    flashlight is with [edge, bono] on wrong side 
-%       state([bono, adam, larry], [edge], 14),    flashlight is with [bono, adam, larry] on target side 
-%       state([adam, larry, edge], [bono], 4),     flashlight is with [adam, larry, edge] on wrong side 
-%       state([bono, edge], [adam, larry], 2),     flashlight is with [bono,edge] on target side 
-%       state([bono, edge, adam, larry], [], 0)]   START, flashlight on wrong side
+% X = [ state([adam, larry, bono, edge], [], 17),  - flashlight is with [adam, larry, bono, edge] on target side 
+%       state([edge, bono], [adam, larry], 15),    - flashlight is with [edge, bono] on wrong side 
+%       state([bono, adam, larry], [edge], 14),    - flashlight is with [bono, adam, larry] on target side 
+%       state([adam, larry, edge], [bono], 4),     - flashlight is with [adam, larry, edge] on wrong side 
+%       state([bono, edge], [adam, larry], 2),     - flashlight is with [bono,edge] on target side 
+%       state([bono, edge, adam, larry], [], 0)]   - START, flashlight on wrong side
 %
 % A state in the search space is always: 
 % 
@@ -72,7 +114,8 @@
 % The BTeam is on the other side of the bridge and has no flashlight.
 % The Clock indicates time at that state.
 
-solve(HistoryOut) :- explore([state([bono,edge,adam,larry],[],0)],HistoryOut).
+solve(HistoryOut) :- 
+    explore([state([bono,edge,adam,larry],[],0)],HistoryOut).
 
 % Explore a list of states (the History) by either detecting a solution or
 % expanding the "top of the History" into possible next states, then recursively
@@ -113,8 +156,11 @@ expand([Current|History],[Next,Current|History]) :-
     union(BTeam,Team,NextBTeam),
     Next = state(NextBTeam,NextATeam,NextClock).
 
-team_selection(Candidates,[A,B]) :- select(A, Candidates, SmallerCands),select(B, SmallerCands, _), A @< B. 
-team_selection(Candidates,[A])   :- select(A, Candidates, _).
+team_selection(Candidates,[A,B]) :- 
+    select(A, Candidates, SmallerCands),select(B, SmallerCands, _), A @< B. 
+
+team_selection(Candidates,[A]) :- 
+    select(A, Candidates, _).
 
 % Stipulate crossing time of "persons"
 
@@ -135,3 +181,4 @@ crossing_time([Who|Rest],Time) :-
     crossing_time(Rest,Time1),
     crossing_time_p(Who,Time2),
     max_list([Time1,Time2],Time).
+
