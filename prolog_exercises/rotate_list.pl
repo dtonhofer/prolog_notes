506e8979b993744e3f947afac25a4c6580cbf508
David Tonhofer <ronerycoder@gluino.name>
2020-04-28 01:46:25 +0200

    Update rotate_list.pl

--- prolog_exercises/rotate_list.pl
+++ prolog_exercises/rotate_list.pl
@@ -1,4 +1,3 @@
-
 % 2345678901234567890123456789012345678901234567890123456789012345678901234567
 % ============================================================================
 % 2020-04-XX
@@ -18,10 +17,16 @@
 % although the solution below, which uses two difference lists and
 % a single scan of the original list (an no append/3), was not proposed.
 % ============================================================================
-
-% ===
 % rotate_list(+List,+N,?Rotated).
-% ===
+%
+% Rotates List by N positions. 
+% N=0 : no rotation
+% N>0 : Rotate "leftwards": move a prefix of (N mod Length) list items to 
+%       the back of the list to form the result.
+% N<0 : Rotate "rightwards": move a suffix of (abs(N) mod Length) list items
+%       to the front of the list to form the result (however, this is done
+%       completely the same way as for the case N>0) 
+% ============================================================================
 
 rotate_list([],_,[]) :- !.
 
@@ -49,21 +54,21 @@ test(empty1) :- rotate_list([], 0,R), R=[].
 test(empty2) :- rotate_list([], 1,R), R=[].
 test(empty3) :- rotate_list([],-1,R), R=[].
 
-test(three3m6) :- rotate_list([1,2,3],-6,R), rotate_list([1,2,3],0,R).
-test(three3m5) :- rotate_list([1,2,3],-5,R), rotate_list([1,2,3],1,R).
-test(three3m4) :- rotate_list([1,2,3],-4,R), rotate_list([1,2,3],2,R).
-test(three3m3) :- rotate_list([1,2,3],-3,R), rotate_list([1,2,3],0,R).
-test(three3m2) :- rotate_list([1,2,3],-2,R), rotate_list([1,2,3],1,R).
-test(three3m1) :- rotate_list([1,2,3],-1,R), rotate_list([1,2,3],2,R).
+test(three3m6) :- rotate_list([1,2,3],-6,R), R=[1,2,3].
+test(three3m5) :- rotate_list([1,2,3],-5,R), R=[2,3,1].
+test(three3m4) :- rotate_list([1,2,3],-4,R), R=[3,1,2].
+test(three3m3) :- rotate_list([1,2,3],-3,R), R=[1,2,3].
+test(three3m2) :- rotate_list([1,2,3],-2,R), R=[2,3,1].
+test(three3m1) :- rotate_list([1,2,3],-1,R), R=[3,1,2].
 test(three3z)  :- rotate_list([1,2,3],+0,R), R=[1,2,3].
 test(three3p1) :- rotate_list([1,2,3],+1,R), R=[2,3,1].
 test(three3p2) :- rotate_list([1,2,3],+2,R), R=[3,1,2].
-test(three3p3) :- rotate_list([1,2,3],+3,R), rotate_list([1,2,3],0,R).
-test(three3p4) :- rotate_list([1,2,3],+4,R), rotate_list([1,2,3],1,R).
-test(three3p5) :- rotate_list([1,2,3],+5,R), rotate_list([1,2,3],2,R).
-test(three3p6) :- rotate_list([1,2,3],+6,R), rotate_list([1,2,3],0,R).
-test(three3p7) :- rotate_list([1,2,3],+7,R), rotate_list([1,2,3],1,R).
-test(three3p8) :- rotate_list([1,2,3],+8,R), rotate_list([1,2,3],2,R).
+test(three3p3) :- rotate_list([1,2,3],+3,R), R=[1,2,3].
+test(three3p4) :- rotate_list([1,2,3],+4,R), R=[2,3,1].
+test(three3p5) :- rotate_list([1,2,3],+5,R), R=[3,1,2].
+test(three3p6) :- rotate_list([1,2,3],+6,R), R=[1,2,3].
+test(three3p7) :- rotate_list([1,2,3],+7,R), R=[2,3,1].
+test(three3p8) :- rotate_list([1,2,3],+8,R), R=[3,1,2].
 
 test(three3zvar)  :- rotate_list([S0,S1,S2],+0,R), R=[S0,S1,S2].
 test(three3p1var) :- rotate_list([S0,S1,S2],+1,R), R=[S1,S2,S0].
