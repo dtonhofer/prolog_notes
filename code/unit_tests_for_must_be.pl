bc906d18d1afe54be2040a3b6e3e04cbca934485
David Tonhofer <ronerycoder@gluino.name>
2020-05-19 23:33:34 +0200

    Update unit_tests_for_must_be.pl

--- code/unit_tests_for_must_be.pl
+++ code/unit_tests_for_must_be.pl
@@ -1,47 +1,119 @@
-:- begin_tests(must_be).
+/*
+
+ Markus Triska writes at Quora:
+
+ https://www.quora.com/If-prolog-were-being-invented-today-with-no-concern-for-backward-compatibility-or-the-existing-standardization-how-would-it-differ-from-standard-prolog?share=1
+
+ "If Prolog were invented today, I think [...] the type-testing predicates
+  like atom/1, integer/1 and compound/1 would (and should) throw instantiation
+  errors if their arguments are not sufficiently instantiated.
+
+  This is also what the original versions of Prolog did. However, DEC 10 Prolog
+  chose to replace instantiation errors by silent failures, and this has been
+  perpetuated in the Edinburgh tradition for type tests including the ISO standard."
+  
+ See also:
+ 
+ https://youtu.be/ZIv0G4b1xBQ?t=123 "Type tests in Prolog"
+
+*/
+
+% ===
+% The unit tests below illustrate the approaches (provided or not)
+% to type testing
+%
+% Run the unit tests with rt(X).
+% ===
+
+:- begin_tests(type_testing_for_atom).
+
+% ===
+% Default Practice in Prolog
+% ===
+
+% Using atom/1 from the standard. Silently fails for wrong types.
+% Observe: 
+% ?- atom(X). "Are there atoms" - "NO!"
+%
+% It's a freshvar     : Should be "I don't know", but (silently) fails
+% It's not a freshvar : It's an atom     : Success.
+%                       It's not an atom : Failure.
+
+test(default_isatom)          :- atom(foo).
+test(default_isnotatom, fail) :- atom(1/137).
+test(default_freshvar, fail)  :- atom(_).
 
 % ---
-% Old school testing for atom-icity
-% Silently fails on fresh variable (instead of throwing).
+% library(error) also provides is_of_type/2, which behaves in the
+% standard way, but takes the name of a type as argument, which is more
+% flexible.
 % ---
 
-test(oldschool_yes)           :- atom(foo).
-test(oldschool_no, fail)      :- atom(1/137).
-test(oldschool_unknown, fail) :- atom(_).
+test(isoftype_isatom)          :- is_of_type(atom,foo).
+test(isoftype_isnotatom, fail) :- is_of_type(atom,1/137).
+test(isoftype_freshvar, fail)  :- is_of_type(atom,_).
 
-% ---
-% must_be/2: Likes to throw too much.
-% Test "must_be_no" had better fail instead of throwing?
-% ---
+% ===
+% "Sufficiently Instantiated" type checks.
+% The type checking predicate throws an error if it cannot decide.
+%
+% The corresponding predicates do not exist. We add atom_si/1
+% for illustration.
+% ===
 
-test(must_be_yes) :-
-   must_be(atom,foo).
+% atom_si/1 where "si" stands for "sufficiently instantiated"
+%
+% It's a freshvar     : Throws an instantiation error ("I don't know", 
+%                       closest to a third logical value that you can get)
+% It's not a freshvar : It's an atom     : Success.
+%                       It's not an atom : Failure.
 
-test(must_be_no, error(type_error(_,_),_)) :-
-   must_be(atom,1/137).
+atom_si(X) :- nonvar(X),!,(atom(X) -> true; false).
+atom_si(X) :- var(X),instantiation_error(X).
 
-test(must_be_unknown, error(instantiation_error)) :-
-   must_be(atom,_).
+test(si_atom_isatom)                               :- atom_si(foo).
+test(si_atom_isnotatom,fail)                       :- atom_si(1/137).
+test(si_atom_freshvar, error(instantiation_error)) :- atom_si(_).
 
-% doesn't run backwards either
+% ===
+% Asserting that a type relation holds for arguments that must be 
+% "sufficiently instantiated" (what this means depends on the type) 
+% to proceed.
+%
+% This is used in checking parameters at library entry points, or
+% among the helper predicates (during development).
+%
+% must_be/2 from library(error)
+% ===
 
-test(must_be_fwd_only, error(instantiation_error)) :-
-   bagof(X, must_be(X,foo), _Types).
+% It's a freshvar     : Throws an instantiation error ("I don't know")
+% It's not a freshvar : It's an atom     : Success.
+%                       It's not an atom : Throws a type error ("NOPE!")
 
-% ---
-% is_of_type/2: Behaves old-school-ish
-% ---
+test(mustbe_atom_isatom)                               :- must_be(atom,foo).
+test(mustbe_atom_isnotatom, error(type_error(_,_),_))  :- must_be(atom,1/137).
+test(mustbe_atom_freshvar, error(instantiation_error)) :- must_be(atom,_).
 
-test(is_of_type_yes)           :- is_of_type(atom,foo).
-test(is_of_type_no, fail)      :- is_of_type(atom,1/137).
-test(is_of_type_unknown, fail) :- is_of_type(atom,_).
+% ===
+% Asserting that an argument can be instantiated to be of some type.
+% This can be used to check parameters to be constrained/bound later
+% in the predicate. In effect, it's an early test for whether an 
+% unification will fail later.
+%
+% can_be/2 
+%
+% The corresponding predicates do not exist. We add an implmentation
+% for "atom" for illustration.
+% ===
 
-% doesn't run backwards
+can_be(atom, X) :- var(X),!.
+can_be(atom, X) :- nonvar(X),!,(atom(X) -> true; type_error(atom,X)).
 
-test(is_of_type_fwd_only, error(instantiation_error)) :-
-   bagof(X, is_of_type(X,foo), _Types).
+test(canbe_atom_isatom)                              :- can_be(atom,foo).
+test(canbe_atom_isnotatom, error(type_error(_,_),_)) :- can_be(atom,1/137).
+test(canbe_atom_freshvar)                            :- can_be(atom,_).
 
-:- end_tests(must_be).
+:- end_tests(type_testing_for_atom).
 
-rt :- run_tests(must_be).
-
+rt(type_testing_for_atom) :- run_tests(type_testing_for_atom).
+ 
