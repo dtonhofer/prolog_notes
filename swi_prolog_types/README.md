b12a3d3612c1fb50b088c63be0e3d26d51d45ef3
David Tonhofer <ronerycoder@gluino.name>
2020-05-26 16:54:57 +0200

    Updated

--- swi_prolog_types/README.md
+++ swi_prolog_types/README.md
@@ -1,25 +1,68 @@
-# SWI-Prolog datatypes
+# About SWI-Prolog datatypes
 
-A tree depicting the built-in "base types" one may encounter in SWI Prolog (which are 
-similary to the types of other Prolog). As there is no way to construct types from the base types 
-these are the only one there are.
+## SWI Prolog datatype tree image
 
-- [graphml file](swi_prolog_types.graphml). The editabel diagram. Edited with the free 8but not open) Java-based
-[yEd](https://www.yworks.com/products/yed) editor (but you have to switch off antialiasing otherwise it feels like driving
-an ocean liner).
-- [PNG file](swi_prolog_types.png). Awkward to use. 4205 x 3055, 8-bit/color RGB.
-- [SVG file](swi_prolog_types.svg). **Best**. Can be visualized in a browser.
+An image depicting the built-in "datatypes" one may encounter in SWI Prolog (which are 
+similary to the types of other Prolog). As there is no way to define new datatypes from these
+base datatypes ([algebraically](https://en.wikipedia.org/wiki/Algebraic_data_type) or
+otherwise), these are the only one there are.
 
-For a description see this SWI-Prolog wiki entry: [SWI-Prolog datatypes](https://eu.swi-prolog.org/datatypes.txt)
+![screenshot](swi_prolog_type_tree/swi_prolog_type_tree_mini.png)
 
-For the type-testing predicates see this SWI-Prolog manual page: [Verify Type of a Term](https://eu.swi-prolog.org/pldoc/man?section=typetest)
+- [graphml file](swi_prolog_type_tree/swi_prolog_type_tree.graphml). **The editable diagram
+  in [graphml](http://graphml.graphdrawing.org/) format.** 
+  Edited with the free (but not open) Java-based [yEd](https://www.yworks.com/products/yed) editor
+  (Note that you have to switch off antialiasing if you use it, otherwise it feels like driving
+   an ocean liner).
+- [PNG file](swi_prolog_type_tree/swi_prolog_type_tree.png). **The diagram in PNG format**, a bit
+  awkward to use. PNG image data, 6704 x 4147, 8-bit/color RGB, non-interlaced.
+- [SVG file](swi_prolog_type_tree/swi_prolog_type_tree.svg). **The diagram in SVG format**. Best! 
+  Can be visualized in a browser and easily panned & zoomed. **Use this**.
+
+## Code
+
+This is Prolog code which follows the datatype tree above to "tag" the elements of a term.
+
+Contains Unit Test code to be run with `rt(_).`.
+
+Examples (all run with `once/1` to close the open choicepoint):
+
+```logtalk
+?- once(tag(X,S)).
+S = var(X).
+
+?- once(tag(100,S)).
+S = int(100).
+
+?- once(tag(100.1,S)).
+S = float(100.1).
+
+?- once(tag(1/3,S)).
+S = compound(/, [gnd], [int(1), int(3)]).
+
+?- once(tag(d{x:1,y:1,z:Z},S)).
+S = dict(d, [nongnd], [atom(x)-int(1), atom(y)-int(1), atom(z)-var(Z)]).
+
+?- once(tag([1,X,2],S)).
+S = lbox([list, nongnd], int(1), lbox([list, nongnd], var(X), lbox([list, gnd], int(2), emptylist))).
+
+?- once(tag(p(X,[1,2]),S)).
+S = compound(p, [nongnd], [var(X), lbox([list, gnd], int(1), lbox([list, gnd], int(2), emptylist))]).
+```
+
+## Reading
+
+- For a description of data types, see this SWI-Prolog wiki entry:
+  [SWI-Prolog datatypes](https://eu.swi-prolog.org/datatypes.txt)
+
+- For the type-testing predicates see this SWI-Prolog manual page:
+  [Verify Type of a Term](https://eu.swi-prolog.org/pldoc/man?section=typetest)
 
 ## Notes
 
 - There is a question on Stack Overflow about this: [What are the data types in Prolog?](https://stackoverflow.com/questions/12038009/what-are-the-data-types-in-prolog)
-- [Logtalk](https://logtalk.org/) has actual datatypes and OO-style message handlers. This is achieved by setting up Prolog Modules around terms, which have the characteristics of objects (Prolog need a proper hierarchical Module system pronto though)
-- The type tests seems to be non-logical but they are if one considers them to have a hidden additional argument: A term representing the "current computational state" of the system. That view probably doesn't help much.
-- The type tests also look like a vestige of something out of [Modal Logic](https://plato.stanford.edu/entries/logic-modal/)
+- [Logtalk](https://logtalk.org/) has actual datatypes and OO-style message handlers. This is achieved by setting up Prolog Modules around terms, which have the characteristics of objects (Prolog need a proper hierarchical Module system)
+- Type tests seems to be non-logical but they are if one considers them to have a hidden additional argument: A term representing the "current computational state" of the system. That view probably doesn't help much.
 
 ### Better type tests
 
@@ -164,4 +207,5 @@ has no type checking; although there is "Typed Clojure" it does not seem to be l
 Among others...
 
 - Paper: [Towards Typed Prolog](https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.456.7365) _Tom Schrijvers, Vitor Santos Costa, Jan Wielemaker, and Bart Demoen_ (2008)
-- https://eu.swi-prolog.org/pack/list?p=type_check (This probabyl is the corresponding pack, but it is dead) 
+- https://eu.swi-prolog.org/pack/list?p=type_check (This probably is the corresponding pack, but it is dead) 
+
---
+++ swi_prolog_types/code/tagging.pl
@@ -0,0 +1,210 @@
+
+:- use_module(library(clpfd)).
+
+:- begin_tests(tagging).
+
+test(tag_var)      :- mono(tag(T)     , var(T)).
+test(tag_int)      :- mono(tag(100)   , int(100)).
+test(tag_float)    :- mono(tag(100.1) , float(100.1)).
+test(tag_prat)     :- mono(tag(1r3)   , prat(1r3)).
+test(slash)        :- mono(tag(1/3)   , compound(/, [gnd], [int(1), int(3)])).
+test(atom_0)       :- mono(tag('')    , atomzero).
+test(atom_1)       :- mono(tag(x)     , atom(x)).
+test(string_0)     :- mono(tag("")    , stringzero).
+test(string_1)     :- mono(tag("x")   , string("x")).
+test(emptylist)    :- mono(tag([])    , emptylist).
+test(compound_0)   :- mono(tag(a())   , compzero(a)).
+test(dictfunctor)  :- dict_functor(F),
+                      mono(tag(F)     ,dictfunctor).
+test(dict)         :- mono(tag(d{x:1,y:1,z:Z}),
+                      dict(d, [nongnd], [atom(x)-int(1), atom(y)-int(1), atom(z)-var(Z)])).
+test(properlist_1) :- mono(tag([1]), lbox([list, gnd], int(1), emptylist)).
+test(properlist_2) :- mono(tag([1,2]), lbox([list, gnd], int(1), lbox([list, gnd], int(2), emptylist))).
+test(properlist_3) :- mono(tag([1,X,2]), lbox([list, nongnd], int(1), lbox([list, nongnd], var(X), lbox([list, gnd], int(2), emptylist)))).
+test(openlist)     :- mono(tag([1,2,3|X]), lbox([nonlist, nongnd], 
+                                                int(1), 
+                                                lbox([nonlist, nongnd], 
+                                                     int(2), 
+                                                     lbox([nonlist, nongnd], 
+                                                          int(3), var(X))))).
+test(badlist)      :- mono(tag([1,2|huh]), lbox([nonlist, gnd], int(1), lbox([nonlist, gnd], int(2), atom(huh)))).
+test(conj_2)       :- mono(tag((A,B)), conj([nongnd], var(A), var(B))).
+test(conj_3)       :- mono(tag((1,2,3)), conj([gnd], int(1), conj([gnd], int(2), int(3)))).
+test(pair_1)       :- mono(tag(A-B), pair([nongnd], var(A), var(B))).
+test(pair_2)       :- mono(tag(1-2), pair([gnd], int(1), int(2))).
+test(compound_1)   :- mono(tag(p(a,b,c)), compound(p, [gnd], [atom(a), atom(b), atom(c)])).
+test(compound_2)   :- mono(tag(p(X,f(b,c))), compound(p, [nongnd], [var(X), compound(f, [gnd], [atom(b), atom(c)])])).
+test(compound_3)   :- mono(tag(p(X,[1,2])), compound(p, [nongnd], [var(X), lbox([list, gnd], int(1), lbox([list, gnd], int(2), emptylist))])).
+
+% ---
+% TODOs
+% ---
+
+% Test cyclic structures. How do I stop the descent?
+
+% test(cyclic_1)     :- R=[1,2|S],S=[3,4|S],tag(S,X).
+
+% Find out whether the compound is a graph or just a tree. Also test for open list
+
+% test(graph)        :- R=f(A,B),A=g(X,Y),B=h(Y,X),tag(R,S).
+
+% ---
+% Collect tagging results; there must be only one!
+% ---
+
+mono(Goal,Result) :- bagof(S, call(Goal,S), [Result]).
+
+:- end_tests(tagging).
+
+% ===
+% Run all tests by issuing query ?- rt(_).
+% ===
+
+rt(tagging) :- run_tests(tagging).
+
+% ===
+% Obtain the dict functor. (Is there a way to write it down directly?)
+% ===
+
+dict_functor(F) :- compound_name_arity(_{},F,_Arity).
+
+% ===
+% Tagging a term. Try to do this without cuts.
+% There is no pretense at efficiency.
+% ===
+
+% var vs nonvar
+
+tag(T,var(T)) :- var(T).
+tag(T,TT)     :- nonvar(T),tag_nonvar(T,TT).
+
+% nonvar: atomic vs compound
+
+tag_nonvar(T,TT) :- atomic(T),tag_atomic(T,TT).
+tag_nonvar(T,TT) :- compound(T),tag_compound(T,TT).
+
+% atomic: blob vs string vs number
+
+tag_atomic(T,TT) :- string(T),tag_string(T,TT).
+tag_atomic(T,TT) :- blob(T,_),tag_blob(T,TT).
+tag_atomic(T,TT) :- number(T),tag_number(T,TT).
+
+% string
+
+tag_string(T,stringzero) :- assertion(\+blob(T,_)),string_length(T,0).
+tag_string(T,string(T))  :- assertion(\+blob(T,_)),string_length(T,L),L>0.
+
+% number: rational vs. float
+
+tag_number(T,float(T)) :- float(T).
+tag_number(T,TT)       :- rational(T,_,_),tag_rational(T,TT).
+
+% rational: integer vs. "non-integer rational"
+
+tag_rational(T,int(T))  :- integer(T).
+tag_rational(T,prat(T)) :- \+integer(T).
+
+% blob: atom vs. anything else
+
+tag_blob(T,TT) :- atom(T),tag_atom(T,TT).
+tag_blob(T,TT) :- \+atom(T),tag_nonatom_blob(T,TT).
+
+% atom
+
+tag_atom(T,atomzero) :- assertion(blob(T,text)),atom_length(T,0).
+tag_atom(T,atom(T))  :- assertion(blob(T,text)),atom_length(T,L),L>0.
+
+% nonatom blob
+
+tag_nonatom_blob(T,TT) :- blob(T,reserved_symbol),tag_reserved_symbol_blob(T,TT).
+tag_nonatom_blob(T,TT) :- \+blob(T,reserved_symbol),tag_foreign_resource_blob(T,TT).
+
+% reserved symbol blob
+
+tag_reserved_symbol_blob([],emptylist).
+tag_reserved_symbol_blob(F,dictfunctor) :- dict_functor(F).
+
+% foreign resource blob
+
+tag_foreign_resource_blob(T,foreign(M,T)) :- blob(T,M).
+
+% compound term, arbitrary arity
+
+tag_compound(T,TT) :- compound_name_arity(T,_,0),tag_zero_arity_compound(T,TT).
+tag_compound(T,TT) :- compound_name_arity(T,_,A),A>0,tag_nonzero_arity_compound(T,TT).
+
+% compound term, zero arity
+
+tag_zero_arity_compound(T,compzero(N)) :- compound_name_arity(T,N,0).
+
+% compound term, nonzero arity, dict
+
+tag_nonzero_arity_compound(T,dict(DictTag,Marks,TgDictArgs)) :-
+   dict_functor(F),
+   compound_name_arguments(T,F,[DictTag|DictArgs]),
+   marks(T,Marks),
+   re_pair(DictArgs,TgDictArgsUnsorted),
+   keysort(TgDictArgsUnsorted,TgDictArgs).
+
+% compound term, arity 2, listbox
+
+tag_nonzero_arity_compound(T,lbox(Marks,TgHead,TgTail)) :-
+   compound_name_arguments(T,'[|]',[Head,Tail]),
+   marks(T,Marks),
+   tag(Head,TgHead),
+   tag(Tail,TgTail).
+
+% compound term, arity 2, conjunction
+
+tag_nonzero_arity_compound(T,conj(Marks,TgL,TgR)) :-
+   compound_name_arguments(T,',',[L,R]),
+   marks(T,Marks),
+   tag(L,TgL),
+   tag(R,TgR).
+
+% compound term, arity 2, pair
+
+tag_nonzero_arity_compound(T,pair(Marks,TgL,TgR)) :-
+   compound_name_arguments(T,'-',[L,R]),
+   marks(T,Marks),
+   tag(L,TgL),
+   tag(R,TgR).
+
+% compound term, none of the above
+
+tag_nonzero_arity_compound(T,compound(N,Marks,TgParams)) :-
+   compound_name_arguments(T,N,Params),
+   length(Params,L),
+   \+ (dict_functor(N),L#=2*_+1), % would be a dict
+   [N,L] \== ['[|]',2],          % would be a listbox
+   [N,L] \== [',',2],            % would be a conjunction
+   [N,L] \== ['-',2],            % would be a pair 
+   marks(T,Marks),
+   maplist(tag,Params,TgParams).
+
+% Helper to associate keys and values of a dict
+
+re_pair([],[]).
+
+re_pair([V,K|VKs],[TgK-TgV|TgVKs]) :-
+   tag(V,TgV),
+   tag(K,TgK),
+   re_pair(VKs,TgVKs).
+
+% Helper to examine compound term substructure
+
+marks(T,M) :- 
+   M0=[],
+   (cyclic_term(T) -> M1=[cyclic|M0] ; M1=M0),
+   (ground(T)      -> M2=[gnd|M1]    ; M2=[nongnd|M1]),
+   (is_list(T)     
+    -> 
+    M3=[list|M2]
+    ; 
+    compound_name_arity(T,'[|]',2)
+    ->
+    M3=[nonlist|M2]
+    ;
+    M3=M2),
+   M=M3.
+
