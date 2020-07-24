c37be4ced9c0b5e80ad9fdc386523f1b50b05b2a
David Tonhofer <ronerycoder@gluino.name>
2020-07-24 15:09:25 +0200

    Updates

--- code/strings/string_of_spaces.pl
+++ code/strings/string_of_spaces.pl
@@ -2,86 +2,55 @@
 % Generating/Recognizing/Verifying strings of spaces
 % ===
 
-string_of_spaces(0,"")     :- !.
-string_of_spaces(1," ")    :- !.
-string_of_spaces(2,"  ")   :- !.
-string_of_spaces(3,"   ")  :- !.
-string_of_spaces(4,"    ") :- !.
+:- consult([conversion]).
 
-% ===
-% string_of_spaces(?N,?Spaces).
-%
-% N      is supposed to be an integer
-% Spaces is supposed to be a String consisting only of " "
-%        Actually type testing is left for lower predicates!
-%  
-% ===
+% ---
+% Actually possibly larege strings generate using string_concat/3
+% Add specific cases besides length 0 and 1 for fats generation for
+% small strings.
+% ---
 
-convert_to_string(X,Str) :- nonvar(X),atom_string(X,Str).
+gen_string_of_spaces( 0,"")           :- !.
+gen_string_of_spaces( 1," ")          :- !.
+gen_string_of_spaces( 2,"  ")         :- !.
+gen_string_of_spaces( 3,"   ")        :- !.
+gen_string_of_spaces( 4,"    ")       :- !.
+gen_string_of_spaces( 5,"     ")      :- !.
+gen_string_of_spaces( 6,"      ")     :- !.
+gen_string_of_spaces( 7,"       ")    :- !.
+gen_string_of_spaces( 8,"        ")   :- !.
+gen_string_of_spaces( 9,"         ")  :- !.
+gen_string_of_spaces(10,"          ") :- !.
 
-string_of_spaces(N,Spaces) :-
-   var(N),nonvar(Spaces),              % case: "determining string length"
-   !,
-   convert_to_string(Spaces,Str),
-
-   string_length(Spaces,Len),          % also accepts atom, number
-   string_of_spaces(SpacesN,Len),      % regenerate spacey string
-   
-
-string_of_spaces(N,Spaces) :-
-   nonvar(N),nonvar(Spaces),
-   string_of_spaces(N,XSpaces),
-   XSpaces == Spaces.
-
-string_of_spaces(N,Spaces) :-
-   nonvar(N),var(Spaces),
-   !,
-   spaces(N,Spaces).
-
-string_of_spaces(N,Spaces) :-
-   var(N),var(Spaces),
-   !
-   generate_string_of_spaces(N,Spaces).
-
-
-% generate_string_of_spaces(0,"").
-% generate_string_of_spaces(N,Spaces) :- 
-%    generate_string_of_spaces(Nsm,SpacesSm),
-%    succ(Nsm,N), 
-%    string_concat(" ",SpacesSm,Spaces).
-
-generate_string_of_spaces(N,Spaces) :-
-   between(0,inf,N),
-   spaces(N,Spaces).
-
-  
- 
-
-
-
-
-string_of_spaces(N,Spaces) :-
-   N>4, !,
+gen_string_of_spaces(N,Spaces) :-
+   N>10, !,
    divmod(N,2,Times,Remainder),
-   string_of_spaces(Times,S1),
+   gen_string_of_spaces(Times,S1),
    string_concat(S1,S1,S2),
    (Remainder>0
     -> (string_of_spaces(Remainder,SR), string_concat(S2,SR,Spaces))
     ;  Spaces = S2).
 
 % ===
-% Unit tests
+% Call this
 % ===
 
-:- begin_tests(spaces).
+string_of_spaces(N,Spaces) :-
+   nonvar(Spaces),                     % case: check or determine length
+   !,
+   convert_to_string(Spaces,Str),      % make sure it's a string for "==" later
+   string_length(Str,N),               % length is now known
+   string_of_spaces(N,StrNew),         % regenerate spacey string
+   Str == StrNew.                      % must be the same (i.e. fail if Spaces is not "spacey")
+   
+string_of_spaces(N,Spaces) :-          
+   var(Spaces),nonvar(N),              % case: generate a string
+   !,
+   gen_string_of_spaces(N,Spaces).
 
-test(multiple) :-
-   forall(between(0,2000,Len),
-          (spaces(Len,Spaces),
-           string_length(Spaces,Len),
-           debug(test_spaces,"Ok for length ~d\n",[Len]))).
-
-:- end_tests(spaces).
-
-
+string_of_spaces(N,Spaces) :-
+   var(Spaces),var(N),                 % case: generate pairs
+   !,
+   between(0,inf,N),
+   gen_string_of_spaces(N,Spaces).
 
---
+++ code/strings/string_of_spaces.plt
@@ -0,0 +1,45 @@
+% ===
+% Generating/Recognizing/Verifying strings of spaces
+% ===
+
+:- consult([string_of_spaces]).
+:- debug(test_string_of_spaces).
+
+:- begin_tests(string_of_spaces).
+
+test("generate a short string", true(Str == "     ")) :- 
+   string_of_spaces(5,Str).
+
+test("generate a long string", true(Str == "               ")) :-
+   string_of_spaces(15,Str).
+
+test("verify a string") :-
+   string_of_spaces(5,"     ").
+
+test("length of string",true(L == 5)) :-
+   string_of_spaces(L,"     ").
+
+test("generate pairs", true(Bag == [0-"",1-" ",2-"  ",3-"   ",4-"    ",5-"      "])) :-
+   length(Bag,5),
+   bagof([L-S],limit(5,string_of_spaces(L,S)),Bag).
+
+test("generate multiple strings") :-
+   forall(between(0,2000,Len),
+          (string_of_spaces(Len,Spaces),
+           string_length(Spaces,Len),
+           debug(test_spaces,"Ok for length ~d\n",[Len]))).
+
+test("fail verifiying a non-space string",fail) :-
+   string_of_spaces(5,"12345").
+
+test("fail verifying length of string",fail) :-
+   string_of_spaces(50,"     ").
+
+test("fail obtaining length of non-space string",fail) :-
+   string_of_spaces(_,"XXX").
+
+:- end_tests(string_of_spaces).
+
+
+
+
