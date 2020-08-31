5f2011266406e97527fd0046068464f7276933ec
David Tonhofer <ronerycoder@gluino.name>
2020-08-31 22:48:17 +0200

    Update swiprologpull.sh

--- compiling/swiprologpull.sh
+++ compiling/swiprologpull.sh
@@ -5,6 +5,31 @@
 # https://mvnrepository.com/artifact/junit/junit/
 
 # ===========================================================================
+# This is free and unencumbered software released into the public domain.
+# 
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+# 
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+# For more information, please refer to <http://unlicense.org/>
+# ===========================================================================
 # swiprologpull.sh
 #
 # I don't like to think when building, so here is a bash shell script to
@@ -627,7 +652,8 @@ build() {
    ninja
 
    # Test
-   # Note that the logfile is just re-written at the end; maybe we should delete it right here first?
+   
+   # Delete the logfile prior to tests.
 
    local logfile="Testing/Temporary/LastTest.log"
 
@@ -648,7 +674,8 @@ build() {
       exit 1
    fi
    
-   # Always grep for ERROR even in case of success
+   # Always grep for ERROR even in case of success. There may also be warnings, but
+   # they are probably of low interest.
 
    if [[ -f "$logfile" ]]; then
       grep ERROR "$logfile"
