4e465844bc4ca7ce1a45a1247fe844145cf08879
David Tonhofer <ronerycoder@gluino.name>
2020-09-12 14:40:13 +0200

    Updates

--- swipl_notes/about_attributed_variables/enum_domain.pl
+++ swipl_notes/about_attributed_variables/enum_domain.pl
@@ -1,11 +1,12 @@
 % ===
-% Rewritten example of 
+% Rewritten example of
 % https://eu.swi-prolog.org/pldoc/man?section=attvar
 % ===
 
 :- module(enum_domain,
-          [ 
-          enum_domain/2      % Var, ?Domain
+          [
+           enum_domain/2      % Var, ?Domain
+          ,enum_domain/3      % For trying attribute merge outside of module
           ]).
 
 :- use_module(library(ordsets)).
@@ -19,11 +20,11 @@
 % ---
 % For readability, I will not use ->/2 all that much. Instead I will use dedicated
 % meta-predicates (these are slow though, so maybe unusable in production). Should
-% really be rewrite instructions. 
+% really be rewrite instructions.
 % ---
 
-if_then_else(Condition,Then,Else) :- 
-   call(Condition) 
+if_then_else(Condition,Then,Else) :-
+   call(Condition)
    -> call(Then)
    ;  call(Else).
 
@@ -43,23 +44,23 @@ switch([If1,Then1],[If2,Then2],Else) :-
 attr_key(enum_domain).
 
 % ---
-% Define the hook predicate to call whenever a variable with "our" attribute is 
+% Define the hook predicate to call whenever a variable with "our" attribute is
 % involved in unification.
 %
 % From the SWI-Prolog reference manual:
 %
 % - - - - - - -
-% "A hook that must be defined in the module to which an attributed variable 
-%  refers. It is called after the attributed variable has been unified with a 
-%  non-var term, possibly another attributed variable. AttValue is the 
+% "A hook that must be defined in the module to which an attributed variable
+%  refers. It is called after the attributed variable has been unified with a
+%  non-var term, possibly another attributed variable. AttValue is the
 %  attribute that was associated to the variable in this module and VarValue
 %  is the new value of the variable. If this predicate fails, the unification
 %  fails. If VarValue is another attributed variable the hook often combines
 %  the two attributes and associates the combined attribute with VarValue
-%  using put_attr/3." 
-% - - - - - - - 
+%  using put_attr/3."
+% - - - - - - -
 %
-% "AttValue" could, in fact, be unbound on call. 
+% "AttValue" could, in fact, be unbound on call.
 %
 % This is the predicate that gets called on unfication.
 % ATTV (attribute value on the old variable) is a list of allowed values for
@@ -69,21 +70,32 @@ attr_key(enum_domain).
 % ---
 
 attr_unify_hook(ATTV,PUV) :-
-   debug(enum_domain,"enum_domain:attr_unify_hook called with ATTV = ~q, PUV = ~q",[ATTV, PUV]),
+  debug_on_entry(ATTV,PUV),
+  assert_on_entry(ATTV),
+  if_then_else(
+      auh_worker(ATTV,PUV),
+      debug_on_success(PUV),
+      (debug(enum_domain,"enum_domain:attr_unify_hook fails",[]),fail)).
+
+debug_on_entry(ATTV,PUV) :-
+   (get_attrs(PUV, ATTPUV) -> true ; ATTPUV = 'none'),
+   debug(enum_domain,"enum_domain:attr_unify_hook called with ATTV = ~q, PUV = ~q, attributes(PUV) = ~q",[ATTV, PUV, ATTPUV]).
+
+assert_on_entry(ATTV) :-
    assertion(nonvar(ATTV)),       % this is not generally the case, but it is in this application
    assertion(is_ordset(ATTV)),    % additionally it's an ordset
-   assertion(maplist(atom,ATTV)), % and additionally, elements are all atoms
-   if_then_else(
-      auh_worker(ATTV,PUV), 
-      (debug(enum_domain,"enum_domain:attr_unify_hook succeeds",[]),true),
-      (debug(enum_domain,"enum_domain:attr_unify_hook fails",[]),fail)),
-   % some additional logging before succeeding (it's fun!)
+   assertion(maplist(atom,ATTV)). % and additionally, elements are all atoms
+
+debug_on_success(PUV) :-
    attr_key(Key),
    if_then_else(
       get_attr(PUV,Key,PUVATTV),  % This succeeds iff PUV denotes a 'hole' with our attribute
-      debug(enum_domain,"enum_domain:attr_unify_hook on successful return, PUV is an unbound variable with attribute = ~q",[PUVATTV]),
-      debug(enum_domain,"enum_domain:attr_unify_hook on successful return, PUV is no longer a variable but ~q",[PUV])).
- 
+      debug(enum_domain,"enum_domain:attr_unify_hook succeeds. PUV is an unbound variable with attribute = ~q",[PUVATTV]),
+      debug(enum_domain,"enum_domain:attr_unify_hook succeeds. PUV is no longer a variable but ~q",[PUV])).
+
+debug_on_failure :-
+   debug(enum_domain,"enum_domain:attr_unify_hook fails",[]).
+
 % ---
 % ATTV (attribute value on the old variable) is a list of allowed values for
 % PUV (the Post-Unification Value).
@@ -106,14 +118,14 @@ attr_unify_hook(ATTV,PUV) :-
 % - Not a hole (PUV is a bound variable)
 %   ... then verify that PUV indeed has a value allowed by ATTV.
 %   ... because what happened was that a hole with our attribute was unified with a value
-%       and we have to veto the unification as the constraint is not fulfuilled. 
+%       and we have to veto the unification as the constraint is not fulfuilled.
 % ---
 
 auh_worker(ATTV,PUV) :-
-   attr_key(Key), 
+   attr_key(Key),
    switch(
       [get_attr(PUV,Key,PUVATTV),            % This succeeds iff PUV denotes a 'hole' with our attribute
-       auh_intersection(ATTV,PUVATTV,PUV)], 
+       auh_intersection(ATTV,PUVATTV,PUV)],
       [var(PUV),put_attr(PUV,Key,ATTV)],     % PUV could still denote a 'hole' without our attribute
       ord_memberchk(PUV,ATTV)).              % Not a 'hole', so check whether value allowed (incl. whether it is an atom)
 
@@ -133,26 +145,26 @@ auh_ax_decision([A],PUV) :-                  % Intersection contains only one al
 
 auh_ax_decision([A1,A2|As],PUV) :-           % Default case: PUV is attributed with the cardinality>=2 intersection
    attr_key(Key),
-   put_attr(PUV,Key,[A1,A2|As]).       
+   put_attr(PUV,Key,[A1,A2|As]).
 
 % ---
 % Translate attributes from this module to residual goals
 % ---
 
 attribute_goals(X) -->
-   { attr_key(Key), 
+   { attr_key(Key),
      get_attr(X, Key, ATTV),
      compound_name_arguments(C,Key,[X|ATTV]) },
    [C].
 
 % ---
-% Either 
+% Either
 % - get the value of attribute "enum_domain" (a list of atoms enumerating the
-%   values allowed) associated to variable X (really, "... associated to the 
-%   'hole' denoted at call time by variable X", but let's keep the english 
+%   values allowed) associated to variable X (really, "... associated to the
+%   'hole' denoted at call time by variable X", but let's keep the english
 %   description conveniently fluent)
-% or 
-% - merge a list of additional atoms with the list of atoms already associated 
+% or
+% - merge a list of additional atoms with the list of atoms already associated
 %   to variable X using attribute "enum_domain". This reduces to setting the list
 %   to the passed list-of-atoms if there is no such attribute yet.
 % ---
@@ -164,11 +176,24 @@ enum_domain(X, ED) :-
    get_attr(X, Key, ED).         % This fails if X does not designate a hole or has no attribute Key
 
 enum_domain(X, ED) :-
-   nonvar(ED),                   % ED bound? Then set the attribute value of the hole denoted by X   
+   nonvar(ED),                   % ED bound? Then set the attribute value of the hole denoted by X
+   !,                            % Unnecessary cut (last clause!) but let's be clear about the commit
    list_to_ord_set(ED, EDOS),    % Sort/uniquify ED to form EDOS
    assertion(maplist(atom,ED)),  % All should be atom
-   attr_key(Key),                % Retrieve Key value
+   attr_key(Key),                % Retrieve Key value, which *must* be the name of this module
    put_attr(Y, Key, EDOS),       % Instead of replacing an existing attribute on X, set it on freshvar Y...
-   X = Y.                        % ...then merge the attributes. *** THIS WON'T TRIGGER THE HOOK ***
+                                 % ... & merge the attributes on X and Y via a triggered call to attr_unify_hook(Key, Y)
+   (get_attrs(X, ATTX) -> true ; ATTX = 'none'),
+   (get_attrs(Y, ATTY) -> true ; ATTY = 'none'),
+   debug(enum_domain,"Prior to unification: X attributes are ~q, Y attributes are ~q",[ATTX,ATTY]),
+   X = Y.                        % *** THIS ONLY TRIGGERS THE HOOK IF X IS NONVAR ***
 
+% Export Y to perform unification & merge outside of module
+
+enum_domain(_, ED, Y) :-
+   nonvar(ED),
+   list_to_ord_set(ED, EDOS),
+   assertion(maplist(atom,ED)),
+   attr_key(Key),
+   put_attr(Y, Key, EDOS).
 
--- swipl_notes/about_attributed_variables/enum_domain.plt
+++ swipl_notes/about_attributed_variables/enum_domain.plt
@@ -1,73 +1,97 @@
 :- use_module(enum_domain).
 :- use_module(library(ordsets)).
 
+:- debug(enum_domain_test).
+
 :- begin_tests(attributed_variables).
 
 
 %% DOES NOT WORK BUT SHOULD!
 
 test("assign attribute with single allowed value to unbound variable", true(X == quux)) :-
-   enum_domain(X,[quux]).
+   debug(enum_domain_test,"assign attribute with single allowed value to unbound variable",[]),
+   enum_domain(X,[quux]). % Uses 2nd clause, but hook is not called
 
+%% ***
+%% Various other tests that pass nicely
+%% ***
 
 % ---
 
 test("retrieve attribute from unattributed unbound variable", fail) :-
-   enum_domain(_X,_OutSet).
+   debug(enum_domain_test,"retrieve attribute from unattributed unbound variable",[]),
+   enum_domain(_X,_OutSet). % Uses 1st clause
 
 test("retrieve attribute from unattributed bound variable", fail) :-
-   X=alfa,enum_domain(X,_OutSet).
+   debug(enum_domain_test,"retrieve attribute from unattributed bound variable",[]),
+   X=alfa,
+   enum_domain(X,_OutSet). % Uses 1st clause
 
 test("retrieve attribute from non-variable", fail) :-
-   enum_domain(alfa,_OutSet).
+   debug(enum_domain_test,"retrieve attribute from non-variable",[]),
+   enum_domain(alfa,_OutSet). % Uses 1st clause
 
 % ---
 
 test("assign attribute to unbound variable", true) :-
-   enum_domain(_X,[foo,bar,baz]).
+   debug(enum_domain_test,"assign attribute to unbound variable",[]),
+   enum_domain(_X,[foo,bar,baz]). % Uses 2nd clause but hook is not called
 
 test("assign attribute to bound variable with disallowed value", fail) :-
-   X=alfa,enum_domain(X,[foo,bar,baz]).
+   debug(enum_domain_test,"assign attribute to bound variable with disallowed value",[]),
+   X=alfa,
+   enum_domain(X,[foo,bar,baz]). % Uses 2nd clause, and hook is called (and fails) *****************
 
 test("assign attribute to bound variable with allowed value", true) :-
-   X=foo,enum_domain(X,[foo,bar,baz]). % YES IT WORKS!
+   debug(enum_domain_test,"assign attribute to bound variable with allowed value",[]),
+   X=foo,
+   enum_domain(X,[foo,bar,baz]). % YES IT WORKS! Uses 2nd clause, and hook is called (and succeeds) **************
 
 test("assign attribute to non-variable with disallowed value", fail) :-
-   enum_domain(alfa,[foo,bar,baz]).
+   debug(enum_domain_test,"assign attribute to non-variable with disallowed value",[]),
+   enum_domain(alfa,[foo,bar,baz]). % Uses 2nd clause, and hook is called (and fails) *****************
 
 test("assign attribute to non-variable with allowed value", true) :-
-   enum_domain(foo,[foo,bar,baz]).
+   debug(enum_domain_test,"assign attribute to non-variable with allowed value",[]),
+   enum_domain(foo,[foo,bar,baz]). % YES IT WORKS! Uses 2nd clause, and hook is called (and succeeds) **************
 
 % ---
 
 test("assign new 'enum domain' to an unbound variable, then retrieve it", true(OutSet == InSetOrd)) :-
-   InSet = [foo_4,bar_4,baz_4],
-   enum_domain(X,InSet),
-   enum_domain(X,OutSet),
+   debug(enum_domain_test,"assign new 'enum domain' to an unbound variable, then retrieve it",[]),
+   InSet = [foo,bar,baz],
+   enum_domain(X,InSet),  % Uses 2nd clause but hook is not called
+   enum_domain(X,OutSet), % Uses 1st clause
    assertion(is_ordset(OutSet)),
    list_to_ord_set(InSet, InSetOrd).
 
 % ---
 
-test("unify two attributed variables with the allowed values equal", true) :-
-   enum_domain(X,[foo,bar,baz]),
-   enum_domain(Y,[foo,bar,baz]),
-   X = Y.
+test("unify two attributed variables with the allowed values equal", true(ATTS == [bar,baz,foo])) :-
+   debug(enum_domain_test,"unify two attributed variables with the allowed values equal",[]),
+   enum_domain(X,[foo,bar,baz]),   % Uses 2nd clause but hook is not called
+   enum_domain(Y,[foo,bar,baz]),   % Uses 2nd clause but hook is not called
+   X = Y,                          % Hook is called and succeeds
+   get_attrs(X,att(_,ATTS,[])).    % ATTS is the ordset of [foo,bar,baz]
 
-test("unify two attributed variables with the allowed values intersection having two values", true) :-
-   enum_domain(X,[foo,bar,quux]),
-   enum_domain(Y,[foo,bar,zool]),
-   X = Y.
+test("unify two attributed variables with the allowed values intersection having two values", true(ATTS == [bar,foo])) :-
+   debug(enum_domain_test,"unify two attributed variables with the allowed values intersection having two values",[]),
+   enum_domain(X,[foo,bar,quux]),  % Uses 2nd clause but hook is not called
+   enum_domain(Y,[foo,bar,zool]),  % Uses 2nd clause but hook is not called
+   X = Y,                          % Hook is called and succeeds
+   get_attrs(X,att(_,ATTS,[])).    % ATTS is the ordset of [foo,bar]
 
 test("unify two attributed variables with the allowed values intersection having one value", true(X == foo)) :-
-   enum_domain(X,[foo,bar,baz]),
-   enum_domain(Y,[foo,quux,zool]),
-   X = Y.
+   debug(enum_domain_test,"unify two attributed variables with the allowed values intersection having one value",[]),
+   enum_domain(X,[foo,bar,baz]),   % Uses 2nd clause but hook is not called
+   enum_domain(Y,[foo,quux,zool]), % Uses 2nd clause but hook is not called
+   X = Y.                          % Hook is called and succeeds, and X is set to the unique value allowed
 
 test("unify two attributed variables with the allowed values intersection being empty", fail) :-
-   enum_domain(X,[foo,bar]),
-   enum_domain(Y,[quux,zool]),
-   X = Y.
+   debug(enum_domain_test,"unify two attributed variables with the allowed values intersection being empty",[]),
+   enum_domain(X,[foo,bar]),       % Uses 2nd clause but hook is not called
+   enum_domain(Y,[quux,zool]),     % Uses 2nd clause but hook is not called
+   X = Y.                          % Hook is called and fails as no solutions left
 
 :- end_tests(attributed_variables).
 
