1ab5beb7f94297a62b35a3682bf421d76428b4ec
David Tonhofer <ronerycoder@gluino.name>
2020-10-21 17:32:27 +0200

    Updated "tagging.pl" with new info

--- swipl_notes/about_swipl_data_types/code/tagging.pl
+++ swipl_notes/about_swipl_data_types/code/tagging.pl
@@ -15,9 +15,12 @@
 % This program implements the "Prolog data type decision tree" at
 % https://github.com/dtonhofer/prolog_notes/tree/master/swi_prolog_types/swi_prolog_type_tree
 %
-% - Efficiency is of no importance, we just want to implement the tree.
-% - Unknown data types should lead to failure. Use assertion/1 to check conditions
-%   as needed.
+% - Efficiency is of no importance, we just want to implement the decision
+%   tree (it is of course also a subsetting tree, so one could decide the
+%   correct "leaf set" immediately with the correct predicate, e.g. atom(X) 
+%   informs us immediately, no need to start at the top; no matter)
+% - Unknown data types should lead to failure. Use assertion/1 to check 
+%   conditions as needed.
 % - Cuts shall not be used. This means (empty) choicepoints stay open on
 %   success. The unit tests are written to check that only one answer is
 %   indeed returned.
@@ -32,47 +35,142 @@
 %
 % test(graph)        :- R=f(A,B),A=g(X,Y),B=h(Y,X),tag(R,S).
 % ============================================================================
+% Manual calls:
+%
+% ?- tag(d{x:1,y:1,z:Z},X).
+% X = dict(atom(d), [nonground], [atom(x)-int(1), atom(y)-int(1), atom(z)-var(Z)]) ;
+% false.
+%
+% ?- tag(foo,X).
+% X = atom(foo) ;
+% false.
+% ============================================================================
 
 :- use_module(library(clpfd)).  
 
 % ===
-% Unit tests; run with ?- rt(_).
+% plunit unit tests; run with ?- run_tests.
 % ===
 
 :- begin_tests(tagging).
 
-test(tag_var)      :- mono(tag(T)     , var(T)).
-test(tag_int)      :- mono(tag(100)   , int(100)).
-test(tag_float)    :- mono(tag(100.1) , float(100.1)).
-test(tag_prat)     :- mono(tag(1r3)   , prat(1r3)).
-test(slash)        :- mono(tag(1/3)   , compound(/, [gnd], [int(1), int(3)])).
-test(atom_0)       :- mono(tag('')    , atomzero).
-test(atom_1)       :- mono(tag(x)     , atom(x)).
-test(string_0)     :- mono(tag("")    , stringzero).
-test(string_1)     :- mono(tag("x")   , string("x")).
-test(emptylist)    :- mono(tag([])    , emptylist).
-test(compound_0)   :- mono(tag(a())   , compzero(a)).
-test(dictfunctor)  :- dict_functor(F),
-                      mono(tag(F)     ,dictfunctor).
-test(dict)         :- mono(tag(d{x:1,y:1,z:Z}),
-                      dict(d, [nongnd], [atom(x)-int(1), atom(y)-int(1), atom(z)-var(Z)])).
-test(properlist_1) :- mono(tag([1]), lbox([list, gnd], int(1), emptylist)).
-test(properlist_2) :- mono(tag([1,2]), lbox([list, gnd], int(1), lbox([list, gnd], int(2), emptylist))).
-test(properlist_3) :- mono(tag([1,X,2]), lbox([list, nongnd], int(1), lbox([list, nongnd], var(X), lbox([list, gnd], int(2), emptylist)))).
-test(openlist)     :- mono(tag([1,2,3|X]), lbox([nonlist, nongnd], 
-                                                int(1), 
-                                                lbox([nonlist, nongnd], 
-                                                     int(2), 
-                                                     lbox([nonlist, nongnd], 
-                                                          int(3), var(X))))).
-test(badlist)      :- mono(tag([1,2|huh]), lbox([nonlist, gnd], int(1), lbox([nonlist, gnd], int(2), atom(huh)))).
-test(conj_2)       :- mono(tag((A,B)), conj([nongnd], var(A), var(B))).
-test(conj_3)       :- mono(tag((1,2,3)), conj([gnd], int(1), conj([gnd], int(2), int(3)))).
-test(pair_1)       :- mono(tag(A-B), pair([nongnd], var(A), var(B))).
-test(pair_2)       :- mono(tag(1-2), pair([gnd], int(1), int(2))).
-test(compound_1)   :- mono(tag(p(a,b,c)), compound(p, [gnd], [atom(a), atom(b), atom(c)])).
-test(compound_2)   :- mono(tag(p(X,f(b,c))), compound(p, [nongnd], [var(X), compound(f, [gnd], [atom(b), atom(c)])])).
-test(compound_3)   :- mono(tag(p(X,[1,2])), compound(p, [nongnd], [var(X), lbox([list, gnd], int(1), lbox([list, gnd], int(2), emptylist))])).
+test("tag an unbound variable") :-
+   mono(tag(T), var(T)).
+
+test("tag an integer") :-
+   mono(tag(100), int(100)).
+
+test("tag a float") :-
+   mono(tag(100.1), float(100.1)).
+
+test("tag a 'proper rational'") :-  % a rational that is not actually an int
+   mono(tag(1r3), prat(1r3)).
+
+test("tag a slash compound term") :-
+   mono(tag(1/3), compound(/, [gnd], [int(1), int(3)])).
+
+test("tag the empty atom") :-
+   mono(tag(''), atomzero).
+
+test("tag a character (an atom of length 1)") :-
+   mono(tag(x), atom(x)).
+
+test("tag the empty string") :-
+   mono(tag(""), stringzero).
+
+test("tag a string of length 0") :-
+   mono(tag("x"), string("x")).
+
+test("tag the empty list") :-
+   mono(tag([]), emptylist).
+
+test("tag a compound of arity 0") :- 
+   mono(tag(a()), compzero(a)).
+
+test("tag a dict functor name") :-
+   dict_functor_name(DFN),
+   mono(tag(DFN), dictfunctorname).
+
+test("tag a dict") :- 
+   mono(
+      tag(d{x:1,y:1,z:Z}),
+      dict(atom(d), [nonground], [atom(x)-int(1), atom(y)-int(1), atom(z)-var(Z)])).
+
+test("tag a dict with a complex tag") :- 
+   Dict=DT{x:1,y:1,z:Z},
+   DT=foo(bar,baz),
+   mono(
+      tag(Dict),
+      dict(compound(foo,[gnd],[atom(bar),atom(baz)]), [nonground], [atom(x)-int(1), atom(y)-int(1), atom(z)-var(Z)])).
+
+test("tag a proper list #1") :- 
+   mono(tag([1]), lbox([list, gnd], int(1), emptylist)).
+
+test("tag a proper list #2") :- 
+   mono(tag([1,2]),
+      lbox([list, gnd], 
+           int(1), 
+           lbox([list, gnd], 
+                int(2), 
+                emptylist))).
+
+test("tag a proper list #3") :- 
+   mono(tag([1,X,2]),
+      lbox([list, nonground],
+           int(1),
+           lbox([list, nonground],
+              var(X),
+              lbox([list, gnd], 
+                 int(2),
+                 emptylist)))).
+
+test("tag an open list") :- 
+   mono(tag([1,2,3|X]), 
+      lbox([nonlist, nonground], 
+           int(1), 
+           lbox([nonlist, nonground], 
+              int(2), 
+              lbox([nonlist, nonground], 
+                 int(3),
+                 var(X))))).
+
+test("badlist") :-
+   mono(tag([1,2|huh]),
+      lbox([nonlist, gnd], 
+         int(1),
+         lbox([nonlist, gnd],
+            int(2),
+            atom(huh)))).
+
+test("tag a conjunction of 2 elements") :-
+   mono(tag((A,B)), 
+      conj([nonground], var(A), var(B))).
+
+test("tag a conjunction of 3 elements") :-
+   mono(tag((1,2,3)), 
+      conj([gnd], 
+         int(1), 
+         conj([gnd], 
+            int(2),
+            int(3)))).
+
+test("tag a pair of unbound variables") :-
+   mono(tag(A-B), pair([nonground], var(A), var(B))).
+
+test("tag a pair of integers") :-
+   mono(tag(1-2), pair([gnd], int(1), int(2))).
+
+test("tag a compound term #1") :-
+   mono(tag(p(a,b,c)), 
+      compound(p, [gnd], [atom(a), atom(b), atom(c)])).
+
+test("tag a compound term #2") :- 
+   mono(tag(p(X,f(b,c))), 
+      compound(p, [nonground], [var(X), compound(f, [gnd], [atom(b), atom(c)])])).
+
+test("tag a compound term #3") :- 
+   mono(tag(p(X,[1,2])), 
+      compound(p, [nonground], [var(X), lbox([list, gnd], int(1), lbox([list, gnd], int(2), emptylist))])).
 
 % ---
 % Collect tagging results; there must be only one!
@@ -82,24 +180,33 @@ mono(Goal,Result) :- bagof(S, call(Goal,S), [Result]).
 
 :- end_tests(tagging).
 
-rt(tagging) :- run_tests(tagging).
-
 % ============================================================================
 % The business end
 % ============================================================================
 
+
 % ===
-% Obtain the dict functor. (Is there a way to write it down directly?)
+% Obtain the special "SWI-Prolog dict functor name". 
+% It is a dedicated blob that is used as functor symbol in the compound term 
+% used in the implementation of the dict under the hood. 
+% It is printed as C'dict' ... but one cannot type that directly.
 % ===
 
-dict_functor(F) :- compound_name_arity(_{},F,_Arity).
+dict_functor_name(DFN) :- compound_name_arity(_{},DFN,_Arity).
+
+% ===
+% Obtain the dict tag (it is also named "tag" and may actually be an 
+% arbitrary term).
+% ===
+
+dict_tag(DictTag) :- compound_name_arity(_{},DictTag,_Arity).
 
 % ===
 % Tagging a term. Try to do this without cuts. There is no pretense at 
 % efficiency.
 % ===
 
-% var vs nonvar (which is really "freshvar vs. anything else"
+% unbound variable vs anything else
 
 tag(T,var(T)) :- var(T).
 tag(T,TT)     :- nonvar(T),tag_nonvar(T,TT).
@@ -125,7 +232,7 @@ tag_string(T,string(T))  :- assertion(\+blob(T,_)),string_length(T,L),L>0.
 tag_number(T,float(T)) :- float(T).
 tag_number(T,TT)       :- rational(T,_,_),tag_rational(T,TT).
 
-% rational: integer vs. "non-integer rational"
+% rational: integer vs. "non-integer rational" (proper rational = prat)
 
 tag_rational(T,int(T))  :- integer(T).
 tag_rational(T,prat(T)) :- \+integer(T).
@@ -148,7 +255,7 @@ tag_nonatom_blob(T,TT) :- \+blob(T,reserved_symbol),tag_foreign_resource_blob(T,
 % reserved symbol blob
 
 tag_reserved_symbol_blob([],emptylist).
-tag_reserved_symbol_blob(F,dictfunctor) :- dict_functor(F).
+tag_reserved_symbol_blob(DFN,dictfunctorname) :- dict_functor_name(DFN).
 
 % foreign resource blob
 
@@ -165,11 +272,12 @@ tag_zero_arity_compound(T,compzero(N)) :- compound_name_arity(T,N,0).
 
 % compound term, nonzero arity, dict
 
-tag_nonzero_arity_compound(T,dict(DictTag,Marks,TgDictArgs)) :-
-   dict_functor(F),
-   compound_name_arguments(T,F,[DictTag|DictArgs]),
+tag_nonzero_arity_compound(T,dict(TgDictTag,Marks,TgDictArgs)) :-
+   dict_functor_name(DFN),
+   compound_name_arguments(T,DFN,[DictTag|DictArgs]),
    marks(T,Marks),
    re_pair(DictArgs,TgDictArgsUnsorted),
+   tag(DictTag,TgDictTag),
    keysort(TgDictArgsUnsorted,TgDictArgs).
 
 % compound term, arity 2, listbox
@@ -201,10 +309,10 @@ tag_nonzero_arity_compound(T,pair(Marks,TgL,TgR)) :-
 tag_nonzero_arity_compound(T,compound(N,Marks,TgParams)) :-
    compound_name_arguments(T,N,Params),
    length(Params,L),
-   \+ (dict_functor(N),L#=2*_+1), % would be a dict
-   [N,L] \== ['[|]',2],          % would be a listbox
-   [N,L] \== [',',2],            % would be a conjunction
-   [N,L] \== ['-',2],            % would be a pair 
+   \+ (dict_functor_name(N),L#=2*_+1), % would be a dict; one can also use is_dict/2
+   [N,L] \== ['[|]',2],                % would be a listbox
+   [N,L] \== [',',2],                  % would be a conjunction
+   [N,L] \== ['-',2],                  % would be a pair 
    marks(T,Marks),
    maplist(tag,Params,TgParams).
 
@@ -222,7 +330,7 @@ re_pair([V,K|VKs],[TgK-TgV|TgVKs]) :-
 marks(T,M) :- 
    M0=[],
    (cyclic_term(T) -> M1=[cyclic|M0] ; M1=M0),
-   (ground(T)      -> M2=[gnd|M1]    ; M2=[nongnd|M1]),
+   (ground(T)      -> M2=[gnd|M1]    ; M2=[nonground|M1]),
    (is_list(T)     
     -> 
     M3=[list|M2]
