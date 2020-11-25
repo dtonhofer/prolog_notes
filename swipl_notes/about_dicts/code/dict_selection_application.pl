36453e140bc8b685173f940db7246e55cd503d56
David Tonhofer <ronerycoder@gluino.name>
2020-11-25 13:23:48 +0100

    Update dict_selection_application.pl

--- swipl_notes/about_dicts/code/dict_selection_application.pl
+++ swipl_notes/about_dicts/code/dict_selection_application.pl
@@ -1,21 +1,28 @@
-% ===
-% A simple application of dict selection
-% 
-% Override defalt parameters with a dict.
-% We pick a value from a possibly large dict full of various parameters, 
-% and fall back to a default if it doesn't exist.
+% ==========
+% Testing SWI-Prolog dicts as described at 
+% https://eu.swi-prolog.org/pldoc/man?section=bidicts
+% ==========
 
-% success of Dict contains a key 'encoding'; then Value contains the value
+% ===
+% A simple application of dict selection: Override default parameters with a dict.
+%
+% We pick a value from a possibly large dict full of various parameter/value pairs,
+% and fall back to a default value if the parameter doesn't exist.
+% ===
 
 pick(encoding,Dict,Value) :-
    (_{encoding:Value} :< Dict) -> true ; Value = 'iso-8859-1'.
 
+% ---
+% Testing the above
+% ---
+
 :- begin_tests(default_values).
    
-test("Use default encoding", true(Enc == 'iso-8859-1')) :-
+test("use default encoding", true(Enc == 'iso-8859-1')) :-
    pick(encoding, _{} , Enc).
    
-test("Override default encoding", true(Enc == 'utf-8')) :-
+test("override default encoding", true(Enc == 'utf-8')) :-
    pick(encoding, _{foo:bar, encoding:'utf-8'}, Enc).
         
 :- end_tests(default_values).
