22644bf0891461aa2822f622d7b57689e3672d0b
David Tonhofer <ronerycoder@gluino.name>
2020-12-04 20:00:43 +0100

    Update is_list_2.pl

--- code/various/is_list_2.pl
+++ code/various/is_list_2.pl
@@ -3,30 +3,39 @@
 % https://opensource.org/licenses/0BSD
 % ===
 
+% Explainer currently at https://eu.swi-prolog.org/pldoc/doc_for?object=is_list/1
+%
+%   - "oclist" stands for "open or proper list (but not unbound variable)"
+%   - "voclist" stands for "open or proper list or unbound variable"
+%   - "canlist" stands for "not currently a proper list but can become one as it's an open list or an unbound variable"
+%   - "true" means "proper (also known as 'closed') list"
+%   - "false" means "absolutely not a list and can't become one"
+
 % ---
 % is_list(@Term,?What)
 % ---
 %
-%                         +-------------------------------------------+
-%                         |           accepted as What                |
-%             +-----------+-----+---+----+-----+------+-------+-------+
-%             | generated | true|var|open|false|oclist|voclist|canlist|
-%             | as What   |     |   |    |     |      |       |       |
-%   +---------+-----------+-----+---+----+-----+------+-------+-------+
-%   | []      | true      |  x  ,   ,    ,     ,  x   ,  x    ,       |
-%   | [a,b,c] | true      |  x  ,   ,    ,     ,  x   ,  x    ,       |
-%   | _       | var       |     , x ,    ,     ,      ,  x    ,  x    |
-%   | [a,b|_] | open      |     ,   , x  ,     ,  x   ,  x    ,  x    |
-%   | [a,b|x] | false     |     ,   ,    ,  x  ,      ,       ,       |
-%   | foo     | false     |     ,   ,    ,  x  ,      ,       ,       |
-%   +---------+-----------+-------------------------------------------+
+%                         +----------------------------------------------+
+%                         |              accepted as What                |
+%             +-----------+--------+---+----+-----+------+-------+-------+
+%             | generated | true   |var|open|false|oclist|voclist|canlist|
+%             | as What   | closed |   |    |     |      |       |       |
+%             |           | proper |   |    |     |      |       |       |
+%   +---------+-----------+--------+---+----+-----+------+-------+-------+
+%   | []      | true      |  x     ,   ,    ,     ,  x   ,  x    ,       |
+%   | [a,b,c] | true      |  x     ,   ,    ,     ,  x   ,  x    ,       |
+%   | _       | var       |        , x ,    ,     ,      ,  x    ,  x    |
+%   | [a,b|_] | open      |        ,   , x  ,     ,  x   ,  x    ,  x    |
+%   | [a,b|x] | false     |        ,   ,    ,  x  ,      ,       ,       |
+%   | foo     | false     |        ,   ,    ,  x  ,      ,       ,       |
+%   +---------+-----------+----------------------------------------------+
 
 
 % memberchk/2 is used to unify the first entry in the memberchk-list with W
 % when W is unbound; and to accept any entry in the memberchk-list otherwise
 
 is_list(T,W) :-
-   (nonvar(W) -> must_be(oneof([true,false,var,open,oclist,voclist,canlist]),W) ; true),
+   (nonvar(W) -> must_be(oneof([true,false,var,open,oclist,voclist,canlist,closed,proper]),W) ; true),
    var(T) 
    ->
    memberchk(W,[var,voclist,canlist]) 
@@ -37,7 +46,7 @@ is_list(T,W) :-
    ;
    T==[]
    ->
-   memberchk(W,[true,oclist,voclist])
+   memberchk(W,[true,oclist,voclist,closed,proper])
    ;
    W=false.
 
@@ -52,7 +61,7 @@ is_listlike([_|Xs],W) :-
    ;
    Xs==[]
    ->
-   memberchk(W,[true,oclist,voclist])
+   memberchk(W,[true,oclist,voclist,closed,proper])
    ;
    W=false.
 
@@ -62,36 +71,35 @@ is_listlike([_|Xs],W) :-
 
 :- begin_tests(is_list_two).
 
-% A direct translation of the above
+% A direct translation of the truth table
 
-%             | generated | true|var|open|false|oclist|voclist|canlist|
+%             | generated |true|closed|proper|var |open |false|oclist|voclist|canlist|
 
 cases(
-  [[ []       , true     , YES , n , n  ,  n  , YES , YES   ,  n   ],
-   [ [a,b,c]  , true     , YES , n , n  ,  n  , YES , YES   ,  n   ],
-   [ _        , var      ,  n  ,YES, n  ,  n  ,  n  , YES   , YES  ],
-   [ [a,b|_]  , open     ,  n  , n ,YES ,  n  , YES , YES   , YES  ],
-   [ [a,b|x]  , false    ,  n  , n , n  , YES ,  n  ,  n    ,  n   ],
-   [ foo      , false    ,  n  , n , n  , YES ,  n  ,  n    ,  n   ]]).
+  [[ []       , true     , yes , yes  , yes ,  n  ,  n  ,  n  , yes  , yes   ,  n   ],
+   [ [a,b,c]  , true     , yes , yes  , yes ,  n  ,  n  ,  n  , yes  , yes   ,  n   ],
+   [ _        , var      ,  n  ,  n   ,  n  , yes ,  n  ,  n  ,  n   , yes   , yes  ],
+   [ [a,b|_]  , open     ,  n  ,  n   ,  n  ,  n  , yes ,  n  , yes  , yes   , yes  ],
+   [ [a,b|x]  , false    ,  n  ,  n   ,  n  ,  n  ,  n  , yes ,  n   ,  n    ,  n   ],
+   [ foo      , false    ,  n  ,  n   ,  n  ,  n  ,  n  , yes ,  n   ,  n    ,  n   ]]).
  
 test("list/2 truth table") :-
    cases(Cases),
    maplist(checkcase,Cases).
 
-checkcase([Example,W,OnTrue,OnVar,OnOpen,OnFalse,OnOclist,OnVoclist,OnCanlist]) :-
+checkcase([Example,W,OnTrue,OnClosed,OnProper,OnVar,OnOpen,OnFalse,OnOclist,OnVoclist,OnCanlist]) :-
    generates(Example,W),
    maplist({Example}/[OnW,W]>>accepts(OnW,Example,W),
-           [OnTrue,OnVar,OnOpen,OnFalse,OnOclist,OnVoclist,OnCanlist],
-           [true  ,var  ,open  ,false  ,oclist  ,voclist  ,canlist]).
+           [OnTrue,OnClosed,OnProper,OnVar,OnOpen,OnFalse,OnOclist,OnVoclist,OnCanlist],
+           [true  ,closed  ,proper  ,var  ,open  ,false  ,oclist  ,voclist  ,canlist]).
 
 generates(Example,W) :-
    must_be(atom,W),
    is_list(Example,WW),
    assertion(WW == W).
  
-accepts('YES', Example,W) :-
-   !, 
-   debug(test_is_list_two,"Received ~q, ~q, ~q",['YES',Example,W]),
+accepts('yes', Example,W) :-
+   debug(test_is_list_two,"Received ~q, ~q, ~q",['yes',Example,W]),
    must_be(atom,W),
    assertion(is_list(Example,W)).   % accepts W for Example
 
