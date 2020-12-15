a1b84bc42049e71062b8817439e1def7d35c032d
David Tonhofer <ronerycoder@gluino.name>
2020-12-15 14:00:39 +0100

    Many changes

--- code/heavycarbon/utils/between_x.pl
+++ code/heavycarbon/utils/between_x.pl
@@ -13,6 +13,8 @@
 
 % A between/3 which accepts unbound variables as Arg1 and Arg2.
 
+% NOTE: The exception throwing is not ISO Standard and pretty experimental and too unstructured.
+
 between_x(Low,High,Value) :-
    what_is_x(Low,Arg1),
    what_is_x(High,Arg2),
@@ -20,8 +22,8 @@ between_x(Low,High,Value) :-
    % let generators will take over!
    % pass Low,High,Value separately to avoid having to extract them for error handling
    decide(Arg1,Arg2,Arg3,Low,High,Value),
-   assertion(integer(Value)),
-   assertion(between(Low,High,Value)).
+   assertion(integer(Value)),              % remove if you are sure it works
+   assertion(between(Low,High,Value)).     % remove if you are sure it works
    
 % Transform argument into a nicer representation
 
@@ -50,11 +52,11 @@ generate_interval_above_fixed_a1(A1,A2,A3) :-
    between(A1,A2,A3).
 
 generate_interval_with_variable_a2(A1,A2,A3) :-
-   ((A1 =< A3) -> true ; non_iso_domain_error(required_arg1_smaller_or_equal_arg3,A1,A2,A3,[A1,'=<',A3],[A1,'>',A3])),
+   ((A1 =< A3) -> true ; non_iso_domain_error(expecting(smaller_or_equal(arg1(A1),arg3(A3))),A1,A2,A3)),
    between(A3,inf,A2).
 
 generate_interval_with_variable_a1(A1,A2,A3) :-
-   ((A3 =< A2) -> true ; non_iso_domain_error(required_arg3_smaller_or_equal_arg2,A1,A2,A3,[A3,'=<',A2],[A3,'>',A2])),
+   ((A3 =< A2) -> true ; non_iso_domain_error(expecting(smaller_or_equal(arg3(A3),arg2(A2))),A1,A2,A3)),
    between(0,inf,D),
    A1 is A3-D.
 
@@ -64,7 +66,7 @@ generate_interval_below_fixed_a3(A1,A3) :-
        
 % Three var: too loose!
 
-decide(var(_),var(_),var(_),Low,High,Value) :- non_iso_instantiation_error(all_variables_unbound,Low,High,Value).
+decide(var(_),var(_),var(_),Low,High,Value) :- non_iso_instantiation_error(expecting(not(all_args_unbound)),Low,High,Value).
 
 % Two var, one int
 
@@ -84,30 +86,30 @@ decide(int(A1),int(A2),int(A3),_,_,_) :- !,between(A1,A2,A3).
 
 % infinity as argument A2 
 
-decide(var(_),infinity,var(_),Low,High,Value) :- non_iso_domain_error(upper_interval_bound_is_infinity,Low,High,Value).
+decide(var(_),infinity,var(_),Low,High,Value) :- non_iso_domain_error(expecting(not(infinity(upper_interval_bound))),Low,High,Value).
 decide(var(A1),infinity,int(A3),_,_,_) :- generate_interval_below_fixed_a3(A1,A3).
 decide(int(A1),infinity,var(A3),_,_,_) :- between(A1,inf,A3). % generate in a standard way
 decide(int(A1),infinity,int(A3),_,_,_) :- between(A1,inf,A3). % accept in a standard way
 
 % Other "infinities"
 
-decide(_,_,infinity,Low,High,Value) :- non_iso_domain_error(value_is_infinity,Low,High,Value).
-decide(infinity,_,_,Low,High,Value) :- non_iso_domain_error(lower_interval_bound_is_infinity,Low,High,Value).
+decide(_,_,infinity,Low,High,Value) :- non_iso_domain_error(expecting(not(infinity(value))),Low,High,Value).
+decide(infinity,_,_,Low,High,Value) :- non_iso_domain_error(expecting(not(infinity(lower_interval_bound))),Low,High,Value).
 
 % Various others
 
-decide(other(_),_,_,Low,High,Value) :- non_iso_domain_error(arg1_is_infinity,Low,High,Value).
-decide(_,other(_),_,Low,High,Value) :- non_iso_domain_error(arg2_is_infinity,Low,High,Value).
-decide(_,_,other(_),Low,High,Value) :- non_iso_domain_error(arg3_is_infinity,Low,High,Value).
+decide(other(_),_,_,Low,High,Value) :- non_iso_type_error(expecting(one_of([var,integer,atom:inf],arg1(Low))),Low,High,Value).
+decide(_,other(_),_,Low,High,Value) :- non_iso_type_error(expecting(one_of([var,integer,atom:inf],arg2(High))),Low,High,Value).
+decide(_,_,other(_),Low,High,Value) :- non_iso_type_error(expecting(one_of([var,integer,atom:inf],arg3(Value))),Low,High,Value).
 
 % Throwing non ISO but at least meaningful exceptions
 
 non_iso_domain_error(Desc,A1,A2,A3) :-
     throw(my_error(domain_error,Desc,info{low:A1, high:A2, value:A3})).
 
-non_iso_domain_error(Desc,A1,A2,A3,Required,Found) :-
-    throw(my_error(domain_error,Desc,info{low:A1, high:A2, value:A3, required: Required, found: Found})).
-    
+non_iso_type_error(Desc,A1,A2,A3) :-
+    throw(my_error(type_error,Desc,info{low:A1, high:A2, value:A3})).
+        
 non_iso_instantiation_error(Desc,A1,A2,A3) :-
     throw(my_error(instantiation_error,Desc,info{low:A1, high:A2, value:A3})).
     
--- swipl_notes/README.md
+++ swipl_notes/README.md
@@ -4,8 +4,13 @@ Based on the SWI-Prolog manual
 
 - [`between/3`](https://eu.swi-prolog.org/pldoc/doc_for?object=between/3)
    - [Notes](about_between), includes code for `between_with_step/4` and `between_x/3` 
-   - [Website text](/website_comment_texts/between.txt)
+   - [Website text](about_between/webmanualtxt/between.txt)
+
+- [`length/1`](https://eu.swi-prolog.org/pldoc/doc_for?object=length/1)
+   - [Notes](about_length), includes code for `probe_length/3` 
+   - [Website text](about_length/webmanualtxt/length.txt)
+
 - [Verify type of a term](https://eu.swi-prolog.org/pldoc/man?section=typetest)
    - [Notes](about_swipl_data_types), includes image of the decision tree
-   - [Website text](/website_comment_texts/type_tree_in_ascii.txt)
+   - [Website text](about_swipl_data_types/webmanualtxt/type_tree_in_ascii.txt)
    
