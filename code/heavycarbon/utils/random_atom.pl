148ba68e4392ca090b1f232634a12571c8df5ecc
David Tonhofer <ronerycoder@gluino.name>
2020-12-27 20:13:39 +0100

    COde rewritten; also uses library(option) now

--- code/heavycarbon/utils/random_atom.pl
+++ code/heavycarbon/utils/random_atom.pl
@@ -1,35 +1,191 @@
+% ============================================================================
+% 2020-09-XX Version 1
+% 2020-12-27 Version 2, using library(option) and otherwise rewritten
+%
+% https://github.com/dtonhofer/prolog_notes/code/heavycarbon/utils/
+% ----------------------------------------------------------------------------
+% ronerycoder@gluino.name (me) says this is licensed under
+% https://opensource.org/licenses/0BSD
+% ============================================================================
+%
+% TODO: Make the documentation visible via pldoc
+%
+% Load using use_module(library('heavycarbon/utils/random_atom.pl')).
+% once the topmost directory has been put on the library path using:
+% assertz(file_search_path(library,'...path.../prolog_notes/code')).
+
+% Below, option defaults are indicated with _underscores_
+
 :- module(heavycarbon_random_atom,
    [
-    random_char/1       % random_char(X)
-   ,random_text/3       % random_text(Text,Len,Options)
-   ,random_atom/3       % random_atom(Text,Len,Options)
-   ,random_string/3     % random_string(Text,Len,Options)
-   ,random_char_list/2  % random_char_list(List,Options)
+    random_char/1       % random_char(X) - generate char
+   ,random_text/2       % random_text(Text,Len) - generate atom of length Len (if given)
+   ,random_text/3       % random_text(Text,Len,Options) - generate atom or string depending
+                        % on options allow_empty(yes|_no_), what_text(string|_atom_)
+   ,random_char_list/2  % random_char_list(Chars,Options) - generate list of chars
+                        % with options allow_empty(yes|_no_)   
+   ,random_char_list/3  % random_char_list(Chars,Options,Tosses) - generate list of chars with details on process
    ]).
 
-:- include(library('heavycarbon/support/meta_helpers_nonmodular.pl')).
-:- use_module(library('heavycarbon/utils/in_prefix.pl')).
+:- use_module(library(option)).
 
 % ===
-% "the_list/2" always returns the same values computed at runtime.
-% Table it so that it is not needlessly executed over and over.
+% "char_list/2" always returns the same values computed at runtime, namely:
+% Chars: the list of chars ['a','b','c',....,'z'].
+% Max:   the maximum allowed 0-based index into the list Chars
+% The predicate is tabled so that it is not needlessly executed over and over.
+% char_list(-Chars,-Max).
 % ===
 
-:- table the_list.
+:- table char_list.
 
-the_list(List,Max) :- 
-   atom_chars('abcdefghijklmnopqrstuvwxyz',List), 
-   length(List,Length), 
-   succ(Max,Length).
+char_list(Chars,Max) :-
+   atom_chars('abcdefghijklmnopqrstuvwxyz',Chars),  % explode atom into list of chars
+   length(Chars,Length),                            % get its length
+   succ(Max,Length).                                % adjust to 0-indexed
 
 % ===
-% Select a uniformly random character from the list given by "the_list/2"
+% Generate text, with the length of the result either specified (if L is bound) or
+% determined (if L is unbound). Option processing is done through library(option).
+% See https://eu.swi-prolog.org/pldoc/man?section=option
+% Options is a list which may or may not contain:
+% - the option what_text(W), where W is one of the atoms 'atom', 'string' (default 'atom')
+% - the option allow_empty(YN), where YN is one of 'yes', 'no' (default 'no')
+% Corresponding "direct-to-atom" and "direct-to-string" predicates also exist.
+% The just look at the option allow_empty(YN)
 % ===
 
-random_char(X) :-                    % EXPORT
-   the_list(List,Max), 
-   random_between(0,Max,N),
-   nth0(N,List,X).
+% EXPORTED
+random_text(Text,Len) :-
+   random_text(Text,Len,[]).         % only default options
+ 
+% EXPORTED
+random_text(Text,Len,Options) :-
+   nonvar(Len),
+   !,
+   length(Chars,Len),                % throws if Len < 0 or not an integer
+   random_char_list(Chars,Options),  % fill the list of known length
+   transform(Chars,Text,Options).    % generate appropriately-typed text
+
+% EXPORTED
+random_text(Text,Len,Options) :-
+   var(Len),
+   !,
+   random_char_list(Chars,Options),  % fill a list of random length (the length results from a markov process)
+   length(Chars,Len),
+   transform(Chars,Text,Options).
+
+% ===
+% Depending on whether Options indicate to generate a 'string' or an 'atom',
+% generate text of the requested type. The default is 'atom'.
+% ===
+
+transform(Chars,Text,Options) :-
+   option(what_text(W),Options,atom),
+   if_then_else(
+      (W==string),                  % non-standard case which must be specified
+      string_chars(Text,Chars),    
+      atom_chars(Text,Chars)).      % else/default case: W==atom or something else or W is var because Options is funky
+
+% ===
+% Actually generate the list of characters.
+% "Chars" can be an unbound variable.
+%    Then:
+%    - The length of the resulting list that is eventually unified with "Chars" is chosen
+%      according to a Markov process.
+%    - The option "allow_empty(YN)" from the "Options" list is considered.
+%      YN is one of 'yes', 'no' (default 'no'). The resulting list may by this means be
+%      allowed to be empty.
+% "Chars" can be a proper list of unbound variables, thus specifying the length.
+%    Then:
+%    - The option "allow_empty(YN)" from the "Options" list is considered; if the
+%      specified length is 0 and "allow_empty(no)" is given, the predicate fails, thus
+%      staying consistent with the case of "Chars" an unbound variable.
+%
+% Interesting thought: These are actually "compressed" clauses for the actual predicate
+% random_char_list(Chars,Length,Options)
+% where Chars is always unbound, an Length may or may not be bound. If Length is
+% bound, the Chars are selected from a smaller subspace of all possible Chars.
+% If one could give a set of Length, how would one choose a Chars from the union of
+% smaller subspaces with correct relative probabilities?
+%
+% There is a 3-arg version, which can only be used if "Chars" is an unbound variable.
+% It also builds a list holding the changing probability for deciding "more characters"
+% and the coin tosses made to make that decision.
+% ---
+
+% EXPORT
+random_char_list(Chars,Options,Tosses) :-
+   var(Chars),
+   !,
+   if_then_else(
+      option(allow_empty(no),Options,no),        % allow_empty(no) first in Options or allow_empty/1 missing from Options
+      (random_char_list_inner(L2,1,Tosses2),
+       random_char(X),Chars=[X|L2],              % correctly start at length "1"
+       Tosses = [1|Tosses2]),
+      (random_char_list_inner(Chars,0,Tosses))). % might generate nothing
+
+% EXPORT
+random_char_list(Chars,Options) :-
+   var(Chars),
+   !,
+   random_char_list(Chars,Options,_). % call the 3-arg predicate, then dump the "tosses"
+
+% EXPORT
+random_char_list(Chars,Options) :-
+   nonvar(Chars),
+   !,
+   must_be(list(var),Chars),               % "an abundance of caution"; enforce a meaningful call!
+   if_then(
+      (option(allow_empty(no),Options,no), % allow_empty(no) first in Options or allow_empty/1 missing from Options
+       length(Chars,0)),                   % and user requested an empty list
+      fail),                               % fail for consitency reasons
+   maplist(random_char,Chars).            % fill list
+
+% ===
+% random_char_list(-Result,+CharsGeneratedAlready)
+%
+% The length of the list is found by throwing a biased coin before each character
+% selection. The coin's probability of saying "one more character" decreases
+% according to a sigmoid that is a function of the number of characters
+% already in the list (it decreases slowly at first, then has an
+% exponential tail going to 0). That gives nicer results than decreasing the
+% probability according to a exponential in the length of already-collected
+% characters, which would be the case by throwing the same biased coin each time.
+%
+% The style below is very imperative but easier to understand than if you
+% separate it into two clauses, onw for "stop criterium reached + cut" and
+% one for "one more character and recursive call". Note that there are two
+% open lists in action to which we append, but written in differing style:
+% "Chars" and "[Toss|Tosses]" (the latter collects the sigmoid floats, just for
+% fun)
+% ===
+
+random_char_list_inner(Chars,CharsGot,[Toss|Tosses]) :-
+   sigmoid_value(CharsGot,P),    % P becomes smaller and smaller as CharsGot becomes larger (but even for 0, it's not 1.0)
+   random(Coin),                 % Coin is a value [0.0..1.0)
+   if_then_else(
+      (P<Coin),                  % Biased decision on whether to stop, become more probably with length
+      (Chars=[],                 % Decision to stop
+       Toss=(P<Coin),
+       Tosses=[]),
+      (random_char(X),           % Otherwise another random character selection
+       Chars=[X|More],           % Construction of result list (note this is exactly the procedure of "appending to an open list"
+       succ(CharsGot,CharsGot2), % We have 1 more character now
+       Toss=(P>=Coin),
+       random_char_list_inner(More,CharsGot2,Tosses))).
+
+% ===
+% Select a "uniformly random" char from the list of chars given by "char_list/2"
+% This is used from inside maplist/1 and the random_char_lits_inner/2 generator.
+% One can call it with instantiated X, but it's probably going to fail...
+% ===
+
+% EXPORTED
+random_char(X) :-
+   char_list(Chars,Max),
+   random_between(0,Max,Selected),
+   nth0(Selected,Chars,X).
 
 % ===
 % Sigmoid expressing decreasing probability "P" of continuing with "one
@@ -37,99 +193,16 @@ random_char(X) :-                    % EXPORT
 % Plot it at https://www.desmos.com/calculator
 % ===
 
-sigmoid_value(L,P) :- 
+sigmoid_value(L,P) :-
    P is 1-(1/(1+exp(2-(L/2)))).
 
-% ---
-% Generate text, with the length of the result either imposed (L bound) or
-% communicated by (L unbound). Options includes the atom 'atom', 'string', 
-% 'nonempty'.
-% ---
+% ===
+% Helpers to beautify conditions. TODO: Move these to a library.
+% ===
 
-random_text(Text,Len,Options) :-     % EXPORT
-   nonvar(Len),
-   !,
-   length(Chars,Len),                % throws if Len < 0 or not an integer
-   random_char_list(Chars,Options),  % fill the list of known length
-   transform(Chars,Text,Options).
+if_then(Condition,Then) :-
+   call(Condition) -> call(Then) ; true.
 
-random_text(Text,Len,Options) :-     % EXPORT
-   var(Len),
-   !,
-   random_char_list(Chars,Options),  % fill a list of random length
-   length(Chars,Len),
-   transform(Chars,Text,Options).
-
-transform(Chars,Text,Options) :- 
-   switch(
-      (option_string(Options),\+option_atom(Options)), string_chars(Text,Chars),
-      (option_atom(Options),\+option_string(Options)), atom_chars(Text,Chars),
-      atom_chars(Text,Chars)   % default if no option or oth options
-   ).
- 
-random_atom(Text,Len,Options) :-    % EXPORT
-   random_text(Text,Len,[atom|Options]).
-
-random_string(Text,Len,Options) :-  % EXPORT
-   random_text(Text,Len,[string|Options]).
-
-% ---
-% Option checkers.
-% ---
-
-option_atom(Options)     :- in_prefix_with_stoplist(atom,[atom,string],Options).
-option_string(Options)   :- in_prefix_with_stoplist(string,[atom,string],Options).
-option_nonempty(Options) :- in_prefix_with_stoplist(nonempty,[],Options).
-
-% ---
-% "List" can be unbound, then the length is chosen randomly (according to a
-% markov process), or else a proper list of unbound variables, thus giving 
-% the length. 
-% "Option" may be a list. If it contains the atom "nonempty", then the 
-% "List" will contain at least 1 character.
-% ---
-
-random_char_list(List,Options) :-   % EXPORT
-   var(List),
-   !,
-   if_then_else(
-      option_nonempty(Options),
-      (random_char_list_inner(L2,1),random_char(X),List=[X|L2]), % correctly start at "1"
-      (random_char_list_inner(List,0))).
- 
-random_char_list(List,Options) :-
-   nonvar(List),
-   !,
-   must_be(list(var),List),
-   if_then(
-     option_nonempty(Options),
-     (length(List,LL),must_be(positive_integer,LL))),
-   maplist(random_char,List).
-
-% ---
-% random_char_list(-Result,+CharsGeneratedAlready)
-%
-% The length of the list is found by throwing a coin before each character
-% selection. The coin's probability of saying "one more character" decreases 
-% according to a sigmoid that is a function of the number of characters 
-% already in the list (it decreases slowly at first, then has an 
-% exponential tail going to 0). That gives nicer results than decreasing the
-% probability according to a exponential in the length of already-collected
-% characters, which would be the case by throwing the same biased coin each time.
-% ---
-
-% *Stop criterium*
-
-random_char_list_inner([],AlreadyGot) :-
-   sigmoid_value(AlreadyGot,P), % P becomes smaller and smaller as AlreadyGot becomes larger (but even for 0, it's not 1.0)
-   random(Toss),                % Between 0.0 =< Toss < 1.0
-   P<Toss,                      % Stop if Toss is above P
-   !.
-
-% *More chars!*
-
-random_char_list_inner([X|More],AlreadyGot) :-
-   random_char(X),
-   succ(AlreadyGot,AlreadyGotNow),
-   random_char_list_inner(More,AlreadyGotNow).
+if_then_else(Condition,Then,Else) :-
+   call(Condition) -> call(Then) ; call(Else).
 
--- code/heavycarbon/utils/random_atom.plt
+++ code/heavycarbon/utils/random_atom.plt
@@ -3,7 +3,7 @@
 % This is not really a test, just code to exercise the random_atom
 % module. There is no failure criterium.
 
-:- debug(random_atom).
+:- debug(random_atom). % print
 
 :- begin_tests(random_atom).
 
@@ -11,69 +11,33 @@ test("exercising random_char/1") :-
    length(R,100),
    maplist(random_char,R),
    debug(random_atom,"random_char delivers: ~q",[R]).
+
+test("random_text/2, produce atom of length 0",fail) :-
+   random_text(_Text,0).
+
+test("random_text/2, produce atom of length 0: if have to set option") :-
+   random_text(_Text,0,[allow_empty(yes)]).
+
+test("random_text/2, produce atom of length 0: can be explicitly disallowed",fail) :-
+   random_text(_Text,0,[allow_empty(no)]).
    
-test("random_text/3, produce atom of length 0") :-
-   random_text(Text,0,[atom]),
-   assertion((atom(Text),atom_length(Text,0))).
-
-test("random_text/3, produce string of length 0") :-
-   random_text(Text,0,[string]),
-   assertion((string(Text),string_length(Text,0))).
-
-test("random_text/3, produce default (atom) of length 0") :-
-   random_text(Text,0,_),
-   assertion((atom(Text),atom_length(Text,0))).
-
-test("random_text/3, produce something of length 0 that cannot be of length 0",[error(_,_)]) :-
-   random_text(_Text,0,[nonempty]).
-
-test("random_atom/3, length 1, with options that do nothing (take 1)") :-
-   random_atom(Text,1,[nonempty,atom]),
-   assertion((atom(Text),atom_length(Text,1))).
-
-test("random_atom/3, length 1, with options that do nothing (take 2)") :-
-   random_atom(Text,1,[nonempty,string]),
-   assertion((atom(Text),atom_length(Text,1))).
-
-test("random_atom/3, length 1, with no options") :-
-   random_atom(Text,1,_),
-   assertion((atom(Text),atom_length(Text,1))).
-
-test("random_atom/3, length 1, with bad options") :-
-   random_atom(Text,1,foo),
-   assertion((atom(Text),atom_length(Text,1))).
-
-test("random_atom/3, demand length 10, run 100x") :- 
+test("random_text/3, demand length 10, run 100x") :- 
    length(R,100),
-   maplist([Text]>>random_atom(Text,10,_),R),
+   maplist([Text]>>random_text(Text,10),R),
    maplist([Text]>>(assertion((atom(Text),atom_length(Text,10)))),R),
    debug(random_atom,"random atoms of length 10: ~q",[R]).
 
-test("random_atom/3, random length, run 100x") :- 
+test("random_text/3, random length, including length 0, run 100x") :- 
    length(R,100),
-   maplist([Text-Len]>>random_atom(Text,Len,_),R),
-   maplist([Text-Len]>>(assertion((atom(Text),atom_length(Text,Len)))),R),
+   maplist([Text-Len]>>random_text(Text,Len,[allow_empty(yes)]),R),
+   maplist([Text-Len]>>(assertion((atom(Text),atom_length(Text,Len),Len>=0))),R),
    debug(random_atom,"random atoms of random length: ~q",[R]).
 
 test("random_atom/3, random length but not length 0, run 100x") :- 
    length(R,100),
-   maplist([Text-Len]>>random_atom(Text,Len,[nonempty]),R),
+   maplist([Text-Len]>>random_text(Text,Len,[allow_empty(no)]),R),
    maplist([Text-Len]>>(assertion((atom(Text),atom_length(Text,Len),Len>0))),R),
    debug(random_atom,"random atoms of random length, but not empty: ~q",[R]).
 
 :- end_tests(random_atom).
-
-% If you run the above (after uncommenting the debug/1 instruction), you get something like:
-%
-%
-% random_char delivers: [f,j,s,d,a,g,c,t,f,s,t,p,u,w,u,c,k,z,l,d,u,f,c,h,w,h,w,c,b,c...
-%
-% random atoms of length 10: [hnighnehat,kewyxmijvz,urkomtlwmx,lxhjfwryjr,xtjkwcrhlq,
-%    guwbgrckfe,qzgmryedvz,kplmjhlmwd,peearerbwa,rlzwjneqlr,cejsyfcxih,vjklwlmdmf,...
-%
-% random atoms of random length: [f-1,cfbq-4,fem-3,tva-3,ety-3,phbrc-5,dp-2,jqmh-4,rtk-3,
-%    x-1,mebg-4,bpqsc-5,''-0,ohwh-4,rjq-3,hm-2,h-1,zenmor-6,oeyl-4,''-0,m-1,lsp-3,v-1,...
-% 
-% random atoms of random length, but not empty: [iydpn-5,bph-3,frdwtu-6,o-1,rb-2,excy-4,
-%    vgum-4,eiwqsid-7,sv-2,qnxza-5,irkq-4,huzj-4,wtd-3,fpvuyn-6,yf-2,bqu-3,xxszly-6,...
 
