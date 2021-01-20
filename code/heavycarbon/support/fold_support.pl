000f1bdf01ba219d1816212b6818b067623350e8
David Tonhofer <ronerycoder@gluino.name>
2021-01-20 18:10:35 +0100

    Updated fold_support.pl

--- code/heavycarbon/support/fold_support.pl
+++ code/heavycarbon/support/fold_support.pl
@@ -4,22 +4,23 @@
              ,all_false/1 % a list contains only "false" atoms
           ]).
 
-andify_rightwards(true,true,true).
-andify_rightwards(true,false,false).
-andify_rightwards(false,true,false).
-andify_rightwards(false,false,false).
+% success means "I can compute this" (and the truth value is in arg 3)
+% whereas failure means "I can'compute this"
 
-andify_negation_rightwards(true,false,true).
-andify_negation_rightwards(true,true,false).
-andify_negation_rightwards(false,true,false).
-andify_negation_rightwards(false,false,false).
+% andify(Element,FromLeft,ToRight)
 
+and_ify(true,true,true)    :- !. % Found "true" and the AND is still "true" --> keep "true"
+and_ify(true,false,false)  :- !. % Exists to allow actually checking the list elements
+and_ify(false,true,false)  :- !. % Exists to allow actually checking the list elements
+and_ify(false,false,false) :- !. % Exists to allow actually checking the list elements
 
-all_true(List) :-
-   foldl([E,FromLeft,ToRight]>>once(andify_rightwards(FromLeft,E,ToRight)),List,true,Out),
-   Out == true.
+% and_not_ify(Element,FromLeft,ToRight)
 
-all_false(List) :-
-   foldl([E,FromLeft,ToRight]>>once(andify_negation_rightwards(FromLeft,E,ToRight)),List,true,Out),
-   Out == true.
+and_not_ify(false,true,true)   :- !. % Found "false" and the AND is still "true" --> keep "true"
+and_not_ify(false,false,false) :- !. % Exists to allow actually checking the list elements
+and_not_ify(true,false,false)  :- !. % Exists to allow actually checking the list elements 
+and_not_ify(true,true,false)   :- !. % Exists to allow actually checking the list elements
+
+all_true(List)  :- foldl(and_ify,List,true,true). % succeeds if the outcome is "true"
+all_false(List) :- foldl(and_not_ify,List,true,true). % succeeds if the outcome is "true"
 
