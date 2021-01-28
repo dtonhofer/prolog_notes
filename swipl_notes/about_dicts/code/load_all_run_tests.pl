cf4b28bd9b398c267bae1db31d5921c76f7003ec
David Tonhofer <ronerycoder@gluino.name>
2021-01-28 15:05:17 +0100

    Create load_all_run_tests.pl

---
+++ swipl_notes/about_dicts/code/load_all_run_tests.pl
@@ -0,0 +1,8 @@
+load_files([
+    'dict_getting_testing.pl'
+   ,'dict_unification_testing.pl'
+   ,'dict_assembly_disassembly_testing.pl'
+   ,'dict_put_dotcall_testing.pl'
+   ,'dict_equality_testing.pl'
+   ,'dict_selection_testing.pl']), 
+   run_tests.
