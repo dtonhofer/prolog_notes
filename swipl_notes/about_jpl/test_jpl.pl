ca145d0fd022a22ccdc47fe365dd0e6bbc5fa459
David Tonhofer <ronerycoder@gluino.name>
2021-03-14 22:51:05 +0100

    Update test_jpl.pl
    Reviewed

--- swipl_notes/about_jpl/test_jpl.pl
+++ swipl_notes/about_jpl/test_jpl.pl
@@ -1,12 +1,19 @@
-% Referenced from https://eu.swi-prolog.org/pldoc/doc_for?object=section(%27packages/jpl.html%27)
+% ----
+% Some simple starter test for the SWI-Prolog-to-Java bridge ("JPL")
+%
+% Referenced from:
+%
+% https://eu.swi-prolog.org/pldoc/doc_for?object=section(%27packages/jpl.html%27)
+% ----
 
-% The instructions may be added to the init.pl file, probably ~/.config/swi-prolog/init.pl
+% The following module-loading instructions may be added to the init.pl
+% file, which should be ~/.config/swi-prolog/init.pl
 
 :- use_module(library(jpl)).
 :- use_module(library(dcg/basics)).
 :- use_module(library(debug)).
 
-% Switch on debug printing for topic "jpl_test", used in this code
+% Switch on debug printing for debug topic "jpl_test", used in this file
 
 :- debug(jpl_tests).
 
@@ -26,27 +33,32 @@ inheritance_path_of_object(JObj,Path) :-
     assertion(jpl_is_object(JObj)),
     jpl_object_to_class(JObj, JClass),
     debug(jpl_tests,"Object is of Class ~q",[JClass]),
-    inheritance_path_of_class(JClass,[],Path).
+    inheritance_path_of_class(JClass,Path).
 
 % ---
-% Given an object, Class its inheritance path
+% Given an object, print its inheritance path
 % ---
 
-inheritance_path_of_class(JClass,Path,PathOut) :-
-    assertion(jpl_is_object(JClass)),                               % it's an object
-    assertion((jpl_object_to_class(JClass,JXClass),
-               jpl_class_to_classname(JXClass,'java.lang.Class'))), % of type java.lang.Class
-    jpl_class_to_classname(JClass,JClassName),
+inheritance_path_of_class(JClass,Path) :-
+    assertion(jpl_is_object(JClass)),                        % it's an object (i.e. not "null")
+    assertion(
+       (jpl_object_to_class(JClass,JXClass),
+        jpl_class_to_classname(JXClass,'java.lang.Class'))), % of type java.lang.Class
+    jpl_class_to_classname(JClass,JClassName),               % the "classname" indicates what object this is about
     debug(jpl_tests,"Class ~q has name ~q",[JClass,JClassName]),
-    jpl_call(JClass,'getSuperclass',[],JSuperClass),
-    inheritance_path_of_class_sub(JSuperClass,[JClassName|Path],PathOut).
+    (class_superclass(JClass,JSuperClass)
+     ->
+     (Path=[JClassName|MorePath],
+      inheritance_path_of_class(JSuperClass,MorePath))
+     ;
+     Path=[JClassName]).
 
-inheritance_path_of_class_sub(JSuperClass,Path,Path) :-
-   jpl_null(JSuperClass),!.
+% this only works in the direction JClass -> JSuperClass
+% fails if there is no superclass
 
-inheritance_path_of_class_sub(JSuperClass,Path,PathOut) :-
-   \+ jpl_null(JSuperClass),!,
-   inheritance_path_of_class(JSuperClass,Path,PathOut).
+class_superclass(JClass,JSuperClass) :-
+   jpl_call(JClass,'getSuperclass',[],JSuperClass),
+   \+ jpl_null(JSuperClass).
 
 % ---
 % plunit tests
@@ -58,15 +70,15 @@ inheritance_path_of_class_sub(JSuperClass,Path,PathOut) :-
 test("Get the version of your JVM") :-
    jpl_call('java.lang.System','getProperty',['java.version'],TxtAtom),
    atom_codes(TxtAtom,TxtCodes),
-   phrase(java_version(V,SV,SSV),TxtCodes,[]),
+   phrase(java_version(V,SV,SSV),TxtCodes),
    debug(jpl_tests,"Java Version: ~q.~q.~q",[V,SV,SSV]),
-   member(V,[7,8,9,10,11,12,13,14]).
+   between(7,15,V).
 
 test("Create a java.time.LocalDate and call its toString() method",true) :-
    jpl_call('java.time.LocalDate','now',[],JInst),
    jpl_call(JInst,'toString',[],TxtAtom),
    atom_codes(TxtAtom,TxtCodes),
-   phrase(java_localdate(_Y,_M,_D),TxtCodes,[]).
+   phrase(java_localdate(_Y,_M,_D),TxtCodes).
 
 test("Create  LocalDate, store it in the Prolog database, retrieve it, then call it") :-
    jpl_call('java.time.LocalDate','now',[],JInst),
@@ -74,10 +86,10 @@ test("Create  LocalDate, store it in the Prolog database, retrieve it, then call
    storage(java_localdate,JInst2),!,           % retrieve from Prolog DB
    jpl_call(JInst2,'toString',[],TxtAtom),
    atom_codes(TxtAtom,TxtCodes),
-   phrase(java_localdate(_Y,_M,_D),TxtCodes,[]).
+   phrase(java_localdate(_Y,_M,_D),TxtCodes).
 
 test("Print the inheritance path of a GregorianCalendar",
-     true(Path == ['java.lang.Object','java.util.Calendar','java.util.GregorianCalendar'])) :-
+     true(Path == ['java.util.GregorianCalendar','java.util.Calendar','java.lang.Object'])) :-
    jpl_new('java.util.GregorianCalendar',[],JGC),
    debug(jpl_tests,"Obtained Java GregorianCalendar = ~q",[JGC]),
    inheritance_path_of_object(JGC,Path).
