c3796bc054042c39a5ad05638ca98f5e6e6627a3
David Tonhofer <ronerycoder@gluino.name>
2021-06-03 15:51:10 +0200

    Update with license text

--- replacement_of_must_be/checks.pl
+++ replacement_of_must_be/checks.pl
@@ -6,16 +6,39 @@
           ,check_that_named/4
           ]).
 
-/*
+/*  MIT License Follows (https://opensource.org/licenses/MIT)
 
-How to load:
+    Copyright 2021 David Tonhofer <ronerycoder@gluino.name>
 
-assertz(file_search_path(library,'/home/rost/Development/PACKING/prolog_code/unpacked')).
-use_module(library(onepointfour_text/checks)).
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files
+    (the "Software"), to deal in the Software without restriction,
+    including without limitation the rights to use, copy, modify, merge,
+    publish, distribute, sublicense, and/or sell copies of the Software,
+    and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
 
+    The above copyright notice and this permission notice shall be 
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 /*
+
+ * **********************************
+ * A replacement for the must_be/2 predicate (which is used to
+ * check preconditions on predicate entry) of Prolog, in particular
+ * SWI-Prolog.
+ *
+ * See: https://eu.swi-prolog.org/pldoc/doc_for?object=must_be/2
+ * **********************************
 
 Checks currently implemented (aliases are indicated with "/")
 
